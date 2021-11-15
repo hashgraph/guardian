3d8879b6851e2fc5fd1a6b61a1bd545e67828de9
stepankirjakov <Stepan.Kirjakov@waveaccess.ru>
2021-11-15 15:52:23 +0400

    add new tests

---
+++ guardian-service/test/dump/did_document.js
@@ -0,0 +1,158 @@
+"use strict";
+
+module.exports.did_document = [{
+    "_id": {
+        "$oid": "6169b4428c3fb50013b24aa5"
+    },
+    "did": "did:hedera:testnet:x719ZpApzxdFA23aUFsDLVjyNHPMeMcYpAcYBmW8nVn;hedera:testnet:fid=0.0.2859201",
+    "document": {
+        "@context": [
+            "https://www.w3.org/ns/did/v1",
+            "https://ns.did.ai/transmute/v1"
+        ],
+        "id": "did:hedera:testnet:x719ZpApzxdFA23aUFsDLVjyNHPMeMcYpAcYBmW8nVn;hedera:testnet:fid=0.0.2859201",
+        "verificationMethod": [
+            {
+                "id": "did:hedera:testnet:x719ZpApzxdFA23aUFsDLVjyNHPMeMcYpAcYBmW8nVn;hedera:testnet:fid=0.0.2859201#did-root-key",
+                "type": "Ed25519VerificationKey2018",
+                "controller": "did:hedera:testnet:x719ZpApzxdFA23aUFsDLVjyNHPMeMcYpAcYBmW8nVn;hedera:testnet:fid=0.0.2859201",
+                "publicKeyBase58": "7DE3CVyBywCSVbu6NqFouAPnEyP2PuiQMmN315rTKcxf"
+            }
+        ],
+        "authentication": "did:hedera:testnet:x719ZpApzxdFA23aUFsDLVjyNHPMeMcYpAcYBmW8nVn;hedera:testnet:fid=0.0.2859201#did-root-key",
+        "assertionMethod": [
+            "#did-root-key"
+        ]
+    },
+    "status": "CREATE",
+    "createDate": {
+        "$date": "2021-10-15T17:02:58.440Z"
+    },
+    "updateDate": {
+        "$date": "2021-10-15T17:03:03.245Z"
+    }
+}, {
+    "_id": {
+        "$oid": "6169b46e8c3fb50013b24aa8"
+    },
+    "did": "did:hedera:testnet:DngCWN5hckWLRYnAijxQz5Q2Gyy6cxjnDgvq2VNbGMJj;hedera:testnet:fid=0.0.2276271",
+    "document": {
+        "@context": [
+            "https://www.w3.org/ns/did/v1",
+            "https://ns.did.ai/transmute/v1"
+        ],
+        "id": "did:hedera:testnet:DngCWN5hckWLRYnAijxQz5Q2Gyy6cxjnDgvq2VNbGMJj;hedera:testnet:fid=0.0.2276271",
+        "verificationMethod": [
+            {
+                "id": "did:hedera:testnet:DngCWN5hckWLRYnAijxQz5Q2Gyy6cxjnDgvq2VNbGMJj;hedera:testnet:fid=0.0.2276271#did-root-key",
+                "type": "Ed25519VerificationKey2018",
+                "controller": "did:hedera:testnet:DngCWN5hckWLRYnAijxQz5Q2Gyy6cxjnDgvq2VNbGMJj;hedera:testnet:fid=0.0.2276271",
+                "publicKeyBase58": "CoReRVJY82rMwPwnbm25yNGgdU4RviggYSUPFdUryD78"
+            }
+        ],
+        "authentication": "did:hedera:testnet:DngCWN5hckWLRYnAijxQz5Q2Gyy6cxjnDgvq2VNbGMJj;hedera:testnet:fid=0.0.2276271#did-root-key",
+        "assertionMethod": [
+            "#did-root-key"
+        ]
+    },
+    "status": "CREATE",
+    "createDate": {
+        "$date": "2021-10-15T17:03:42.445Z"
+    },
+    "updateDate": {
+        "$date": "2021-10-15T17:03:47.034Z"
+    }
+}, {
+    "_id": {
+        "$oid": "6169b4938c3fb50013b24aab"
+    },
+    "did": "did:hedera:testnet:4iCPiUyiKcHaMLrytqRHavQRCknLGg4n3BcLc5unxn5K;hedera:testnet:fid=0.0.2859201",
+    "document": {
+        "@context": [
+            "https://www.w3.org/ns/did/v1",
+            "https://ns.did.ai/transmute/v1"
+        ],
+        "id": "did:hedera:testnet:4iCPiUyiKcHaMLrytqRHavQRCknLGg4n3BcLc5unxn5K;hedera:testnet:fid=0.0.2859201",
+        "verificationMethod": [
+            {
+                "id": "did:hedera:testnet:4iCPiUyiKcHaMLrytqRHavQRCknLGg4n3BcLc5unxn5K;hedera:testnet:fid=0.0.2859201#did-root-key",
+                "type": "Ed25519VerificationKey2018",
+                "controller": "did:hedera:testnet:4iCPiUyiKcHaMLrytqRHavQRCknLGg4n3BcLc5unxn5K;hedera:testnet:fid=0.0.2859201",
+                "publicKeyBase58": "D2twjJruurR43XCyQEGNPWNjk4DcoqPyCpCfBVpSZ93d"
+            }
+        ],
+        "authentication": "did:hedera:testnet:4iCPiUyiKcHaMLrytqRHavQRCknLGg4n3BcLc5unxn5K;hedera:testnet:fid=0.0.2859201#did-root-key",
+        "assertionMethod": [
+            "#did-root-key"
+        ]
+    },
+    "status": "CREATE",
+    "createDate": {
+        "$date": "2021-10-15T17:04:19.918Z"
+    },
+    "updateDate": {
+        "$date": "2021-10-15T17:04:26.959Z"
+    }
+}, {
+    "_id": {
+        "$oid": "616d72f82ef2bc00141bbe8e"
+    },
+    "did": "did:hedera:testnet:4AGt9SZtL2ybX9Hki9pqq3TgGSzy7BXh6XQLjPdinDku;hedera:testnet:fid=0.0.2276271",
+    "document": {
+        "@context": [
+            "https://www.w3.org/ns/did/v1",
+            "https://ns.did.ai/transmute/v1"
+        ],
+        "id": "did:hedera:testnet:4AGt9SZtL2ybX9Hki9pqq3TgGSzy7BXh6XQLjPdinDku;hedera:testnet:fid=0.0.2276271",
+        "verificationMethod": [
+            {
+                "id": "did:hedera:testnet:4AGt9SZtL2ybX9Hki9pqq3TgGSzy7BXh6XQLjPdinDku;hedera:testnet:fid=0.0.2276271#did-root-key",
+                "type": "Ed25519VerificationKey2018",
+                "controller": "did:hedera:testnet:4AGt9SZtL2ybX9Hki9pqq3TgGSzy7BXh6XQLjPdinDku;hedera:testnet:fid=0.0.2276271",
+                "publicKeyBase58": "AN93B6m1UWoTUMmnHjiZugPR6fPgV4zF83w7zUDngnXE"
+            }
+        ],
+        "authentication": "did:hedera:testnet:4AGt9SZtL2ybX9Hki9pqq3TgGSzy7BXh6XQLjPdinDku;hedera:testnet:fid=0.0.2276271#did-root-key",
+        "assertionMethod": [
+            "#did-root-key"
+        ]
+    },
+    "status": "CREATE",
+    "createDate": {
+        "$date": "2021-10-18T13:13:28.577Z"
+    },
+    "updateDate": {
+        "$date": "2021-10-18T13:13:33.228Z"
+    }
+}, {
+    "_id": {
+        "$oid": "616ddb2e2ef2bc00141bbe93"
+    },
+    "did": "did:hedera:testnet:8NrYtVbU9opPNYZuEA4Zie4Bjy2wmBWn9eQY4YDmfSZ;hedera:testnet:fid=0.0.2859201",
+    "document": {
+        "@context": [
+            "https://www.w3.org/ns/did/v1",
+            "https://ns.did.ai/transmute/v1"
+        ],
+        "id": "did:hedera:testnet:8NrYtVbU9opPNYZuEA4Zie4Bjy2wmBWn9eQY4YDmfSZ;hedera:testnet:fid=0.0.2859201",
+        "verificationMethod": [
+            {
+                "id": "did:hedera:testnet:8NrYtVbU9opPNYZuEA4Zie4Bjy2wmBWn9eQY4YDmfSZ;hedera:testnet:fid=0.0.2859201#did-root-key",
+                "type": "Ed25519VerificationKey2018",
+                "controller": "did:hedera:testnet:8NrYtVbU9opPNYZuEA4Zie4Bjy2wmBWn9eQY4YDmfSZ;hedera:testnet:fid=0.0.2859201",
+                "publicKeyBase58": "9HfLrkUqt72cPxMaQgicnEu7AAoWP8YsRQ9sABGh7UWh"
+            }
+        ],
+        "authentication": "did:hedera:testnet:8NrYtVbU9opPNYZuEA4Zie4Bjy2wmBWn9eQY4YDmfSZ;hedera:testnet:fid=0.0.2859201#did-root-key",
+        "assertionMethod": [
+            "#did-root-key"
+        ]
+    },
+    "status": "CREATE",
+    "createDate": {
+        "$date": "2021-10-18T20:38:06.940Z"
+    },
+    "updateDate": {
+        "$date": "2021-10-18T20:38:11.898Z"
+    }
+}];
--- guardian-service/test/trust-chain.service.js
+++ guardian-service/test/trust-chain.service.js
@@ -2,6 +2,14 @@ const { expect, assert } = require('chai');
 const { trustChainAPI } = require('../dist/api/trust-chain.service');
 const { createChannel, createTable } = require('./helper');
 
+const { did_document } = require('./dump/did_document');
+const { vc_document } = require('./dump/vc_document');
+const { vp_document } = require('./dump/vp_document');
+
+const { testObject1, testObject2, testObject3 } = require('./mockup/trust-chain.service');
+
+
+
 describe('Trust Chain service', function () {
     let service, channel;
 
@@ -9,13 +17,49 @@ describe('Trust Chain service', function () {
 
     before(async function () {
         channel = createChannel();
+
         const didDocumentRepository = createTable();
         const vcDocumentRepository = createTable();
         const vpDocumentRepository = createTable();
-        // vcDocumentRepository.find
-        // vcDocumentRepository.findOne
-        // didDocumentRepository.find
-        // vpDocumentRepository.findOne
+
+        didDocumentRepository.find = async function (param) {
+            const result = did_document.filter(e => e.did == param.where.did['$eq']);
+            return result;
+        }
+
+        vcDocumentRepository.findOne = async function (param) {
+            let result = vc_document;
+            if (param.where.hash) {
+                result = result.filter(e => e.hash == param.where.hash['$eq']);
+            }
+            if (param.where.type) {
+                result = result.filter(e => e.type == param.where.type['$eq']);
+            }
+            if (param.where.policyId) {
+                result = result.filter(e => e.policyId == param.where.policyId['$eq']);
+            }
+            if (param.where.owner) {
+                result = result.filter(e => e.owner == param.where.owner['$eq']);
+            }
+            return result[0];
+        }
+
+        vcDocumentRepository.find = async function (param) {
+            const result = vc_document.filter(e => e.document.credentialSubject[0]['id'] == param.where['document.credentialSubject.id']['$eq']);
+            return result;
+        }
+
+        vpDocumentRepository.findOne = async function (param) {
+            let result = vp_document;
+            if (param.where.hash) {
+                result = result.filter(e => e.hash == param.where.hash['$eq']);
+            }
+            if (param.where['document.id']) {
+                result = result.filter(e => e.document.id == param.where['document.id']['$eq']);
+            }
+            return result[0];
+        }
+
         service = trustChainAPI(channel,
             didDocumentRepository,
             vcDocumentRepository,
@@ -28,7 +72,24 @@ describe('Trust Chain service', function () {
     });
 
     it('Test GET_CHAIN', async function () {
-        // assert.fail();
+        let value;
+        value = await channel.run(GET_CHAIN, '5d46d1fd-75ad-4073-ade7-322d6f31374c');
+        assert.deepEqual(value, testObject1);
+
+        value = await channel.run(GET_CHAIN, '0123583c-ce26-407e-a079-8b72c2fe435c');
+        assert.deepEqual(value, testObject2);
+
+        value = await channel.run(GET_CHAIN, '6i28MnZRhBjw7gjCn3VFeFtEZ3h6CJcrHPpmybvjsHXB');
+        assert.deepEqual(value, testObject3);
+
+        testObject1[0].label= "HASH";
+        testObject1[0].id= "6FNz1t4eHNDncM1zn6J8djJLPnjuhQq3hp6EmcF1ictB";
+        value = await channel.run(GET_CHAIN, '6FNz1t4eHNDncM1zn6J8djJLPnjuhQq3hp6EmcF1ictB');
+        assert.deepEqual(value, testObject1);
+
+        testObject2[0].label= "HASH";
+        testObject2[0].id= "5LDizXoaXcqYmdbGUfsRrzf5PpgWmiUuarmTNgTxAYyU";
+        value = await channel.run(GET_CHAIN, '5LDizXoaXcqYmdbGUfsRrzf5PpgWmiUuarmTNgTxAYyU');
+        assert.deepEqual(value, testObject2);
     });
 });
-
