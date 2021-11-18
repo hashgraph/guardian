d513f319ea08da27da626bd09efc095278bd3d5e
simvalery <valeriy.simonov@envisionblockchain.com>
2021-11-18 18:28:33 +0300

    update hedera-network test script

--- vc-modules/test/test.js
+++ vc-modules/test/test.js
@@ -18,21 +18,25 @@ describe("Hedera", function () {
     it('Test ', async function () {
         const OPERATOR_ID = "0.0.1548173";
         const OPERATOR_KEY = "302e020100300506032b657004220420e749aa65835ce90cab1cfb7f0fa11038e867e74946abca993f543cf9509c8edc";
-    
+
         const client = Client.forTestnet();
         client.setOperator(OPERATOR_ID, OPERATOR_KEY);
-        
-        for (let i = 0; i < 10; i++) {
+
+        let success = 0, failed = 0;
+
+        for (let i = 0; i < 20; i++) {
             try {
                 console.log("execute ", i);
                 const query = new AccountBalanceQuery().setAccountId(OPERATOR_ID);
                 const accountBalance = await query.execute(client);
                 console.log(!!accountBalance);
                 await wait(2000);
+                ++success;
             } catch (error) {
-                console.error(error)
+                console.error(error);
+                ++failed;
             }
         }
-        console.log("end");
+        console.log("end", 'success:', success, 'failed:', failed);
     });
 });
