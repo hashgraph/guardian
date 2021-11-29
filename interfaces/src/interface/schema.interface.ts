5ea808e33bfd1a7534337e24b7f5164d03f7ed92
stepankirjakov <Stepan.Kirjakov@waveaccess.ru>
2021-11-29 18:14:50 +0400

    new schema format
    Signed-off-by: stepankirjakov <Stepan.Kirjakov@waveaccess.ru>

--- interfaces/src/interface/schema.interface.ts
+++ interfaces/src/interface/schema.interface.ts
@@ -1,11 +1,12 @@
 import {SchemaEntity} from '../type/schema-entity.type';
 import {SchemaStatus} from '../type/schema-status.type';
+import {ISchemaDocument} from './schema-document.interface';
 
 export interface ISchema {
     id: string;
-    type: string;
+    name: string;
     entity: SchemaEntity;
     status: SchemaStatus;
     readonly: boolean;
-    document: any;
+    document: ISchemaDocument;
 }
