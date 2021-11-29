2013a5fd05d1b005e7d79c4204954071e83cb623
simvalery <valeriy.simonov@envisionblockchain.com>
2021-11-29 17:23:32 +0300

    decorators tests

---
+++ ui-service/tests/blocks/information-block.test.js
@@ -0,0 +1,10 @@
+require('module-alias/register');
+const {assert} = require('chai');
+
+describe('Information block', function() {
+    it('init', async function () {
+    })
+
+    it('getData', async function() {
+    })
+})
