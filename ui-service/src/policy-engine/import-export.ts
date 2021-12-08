6a195a3c2c36d0fd99e1269b61ec22311d56c6be
simvalery <valeriy.simonov@envisionblockchain.com>
2021-12-08 17:05:42 +0300

    get all for policy

--- ui-service/src/policy-engine/import-export.ts
+++ ui-service/src/policy-engine/import-export.ts
@@ -1,20 +1,37 @@
 import {Response, Router} from 'express';
 import {AuthenticatedRequest} from '../auth/auth.interface';
 import JSZip from 'jszip'
+import {getMongoRepository} from 'typeorm';
+import {Policy} from '@entity/policy';
+import {Guardians} from '@helpers/guardians';
+import {findAllEntities} from '@helpers/utils';
 
 export const importExportAPI = Router();
 
 const FILENAME = 'policy.zip'
 
-importExportAPI.get('/export', async (req: AuthenticatedRequest, res: Response) => {
-    const zip = new JSZip();
-    zip.file('schema1.json', '{"test": "this is test1"}');
-    zip.file('schema2.json', '{"test": "this is test2"}');
+importExportAPI.get('/export/:policyId', async (req: AuthenticatedRequest, res: Response) => {
+    const policy = await getMongoRepository(Policy).findOne(req.params.policyId);
+    const guardians = new Guardians();
 
-    const arcStream = zip.generateNodeStream();
+    const [schemas, tokens] = await Promise.all([
+        Promise.all(findAllEntities(policy.config, 'schema').map(id => guardians.getSchemes({type: id}))),
+        Promise.all(findAllEntities(policy.config, 'tokenId').map(id => guardians.getTokens({tokenId: id})))
+    ]);
 
-    res.setHeader('Content-disposition', `attachment; filename=${FILENAME}`);
-    res.setHeader('Content-type', 'application/zip');
-    // console.log(archive)
-    arcStream.pipe(res);
+    res.json({
+        schemas,
+        tokens,
+        policy
+    })
+
+    // const zip = new JSZip();
+    // zip.file('schema1.json', '{"test": "this is test1"}');
+    // zip.file('schema2.json', '{"test": "this is test2"}');
+    //
+    // const arcStream = zip.generateNodeStream();
+    //
+    // res.setHeader('Content-disposition', `attachment; filename=${FILENAME}`);
+    // res.setHeader('Content-type', 'application/zip');
+    // arcStream.pipe(res);
 });
