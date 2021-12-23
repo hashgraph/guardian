d9c34db396e64eaf2a0fefcc4771342a346950ba
artembuslaev <buslaew.ar@mail.ru>
2021-12-23 13:47:09 +0400

    adding schema form validation
    Signed-off-by: artembuslaev <buslaew.ar@mail.ru>

--- frontend/src/app/components/schema-form/schema-form.component.ts
+++ frontend/src/app/components/schema-form/schema-form.component.ts
@@ -1,6 +1,7 @@
-import { Component, EventEmitter, Input, OnInit, Output, SimpleChanges } from '@angular/core';
-import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
 import { Schema, SchemaField } from 'interfaces';
+import * as moment from 'moment';
 
 /**
  * Form built by schema
@@ -51,7 +52,8 @@ export class SchemaFormComponent implements OnInit {
         if (item.isRef) {
             listItem.control = new FormGroup({});
         } else {
-            listItem.control = new FormControl("", item.required ? Validators.required : null);
+            let validators = this.getValidators(item);
+            listItem.control = new FormControl("", validators);
         }
         item.list.push(listItem);
         item.control.push(listItem.control);
@@ -105,10 +107,14 @@ export class SchemaFormComponent implements OnInit {
                 isArray: field.isArray,
                 isRef: field.isRef,
                 hide: false,
-                context: field.context
+                context: field.context,
+                type: field.type,
+                format: field.format,
+                pattern: field.pattern
             }
             if (!field.isArray && !field.isRef) {
-                item.control = new FormControl("", field.required ? Validators.required : null);
+                let validators = this.getValidators(item);
+                item.control = new FormControl("", validators);
                 group[field.name] = item.control;
             }
             if (!field.isArray && field.isRef) {
@@ -149,5 +155,85 @@ export class SchemaFormComponent implements OnInit {
             this.options.addControl("type", new FormControl(this.context.type));
             this.options.addControl("@context", new FormControl(this.context.context));
         }
+    }
+
+    onDateFieldChange(
+      control: FormControl | null,
+      group: FormGroup | null,
+      controlName: string | null,
+      prepareDate: boolean
+    ): void {
+      let groupControl = control;
+
+      if (group && controlName)
+      {
+        groupControl = group.get(controlName) as FormControl;
+      }
+
+      if (!groupControl)
+      {
+        return;
+      }
+
+      let momentDate = moment(groupControl.value);
+
+      if (!momentDate.isValid())
+      {
+        return;
+      }
+
+      let formatDate =  prepareDate
+       ? momentDate.format("YYYY-MM-DD")
+       : momentDate.toISOString();
+
+      groupControl.setValue(formatDate,
+      {
+        onlySelf: true,
+        emitEvent: false,
+        emitModelToViewChange: false,
+        emitViewToModelChange: false
+      });
+    }
+
+    private getValidators(item:any): ValidatorFn[] {
+      const validators = [];
+
+      if (item.required)
+      {
+        validators.push(Validators.required);
+      }
+
+      if (item.pattern)
+      {
+        validators.push(Validators.pattern(item.pattern));
+        return validators;
+      }
+
+      if (item.format === 'email')
+      {
+        validators.push(Validators.pattern(/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/));
+      }
+
+      if (item.type === 'number')
+      {
+        validators.push(Validators.pattern(/^-?\d*(\.\d+)?$/));
+      }
+
+      if (item.format === 'duration')
+      {
+        validators.push(Validators.pattern(/^[0-9]+$/));
+      }
+
+      if (item.type === 'integer')
+      {
+        validators.push(Validators.pattern(/^-?\d*$/));
+      }
+
+      if (item.format === 'url')
+      {
+        validators.push(Validators.pattern(/^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/));
+      }
+
+      return validators;
     }
 }
