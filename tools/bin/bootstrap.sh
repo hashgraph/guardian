197fb11976391971334ba574c8bc527b2a956e4f
Paul Li <engineforce@gmail.com>
2022-01-20 10:32:26 +1100

    Fix CI

--- tools/bin/bootstrap.sh
+++ tools/bin/bootstrap.sh
@@ -4,7 +4,7 @@ set -e
 
 node --version | grep 16 || { echo "Please use node 16"; false; }
 
-lerna bootstrap --no-private --concurrency ${CONCURRENCY:-2} --include-dependencies "$@"
+lerna bootstrap --no-ci --no-private --concurrency ${CONCURRENCY:-2} --include-dependencies "$@"
 
 # Need to restore symlinks with `file:`` specifier
 # Refer to https://github.com/lerna/lerna/issues/1679
