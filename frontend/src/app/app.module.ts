be81cd954d1bf11d940f1a4cee9263f1255ae28b
Stepan Kirjakov <84465157+Stepan-Kirjakov@users.noreply.github.com>
2022-01-21 14:19:01 +0400

    Update app.module.ts
    fix merge

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -140,7 +140,7 @@ import { VersionSchemaDialog } from './components/dialogs/version-schema/version
         RolesConfigComponent,
         RolesBlockComponent,
         SchemaFormViewComponent,
-        VcDocumentViewComponent
+        VcDocumentViewComponent,
         VersionSchemaDialog
     ],
     imports: [
