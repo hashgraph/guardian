521eaae38a7682e8cbd20084c3a670188db13060
Stepan Kiryakov <stepan.kiryakov@envisionblockchain.com>
2022-01-24 20:04:24 +0400

    fix schema link

--- frontend/src/app/components/document-view/document-view.component.ts
+++ frontend/src/app/components/document-view/document-view.component.ts
@@ -5,54 +5,52 @@ import { Schema } from 'interfaces';
  * View document
  */
 @Component({
-    selector: 'app-document-view',
-    templateUrl: './document-view.component.html',
-    styleUrls: ['./document-view.component.css'],
-    changeDetection: ChangeDetectionStrategy.OnPush
+  selector: 'app-document-view',
+  templateUrl: './document-view.component.html',
+  styleUrls: ['./document-view.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class DocumentViewComponent implements OnInit{
-    @Input('document') document: any;
-    @Input('hide-fields') hideFields!: { [x: string]: boolean };
-    @Input('schemas') schemas!: Schema[];
-    @Input('type') type!: 'VC' | 'VP';
+export class DocumentViewComponent implements OnInit {
+  @Input('document') document: any;
+  @Input('hide-fields') hideFields!: { [x: string]: boolean };
+  @Input('schemas') schemas!: Schema[];
+  @Input('type') type!: 'VC' | 'VP';
 
-    subjects: any[] = []
-    proofJson!: string;
+  subjects: any[] = []
+  proofJson!: string;
 
-    constructor() { }
+  constructor() { }
 
-    ngOnInit(): void {
-      this.proofJson = this.document.proof
-        ? JSON.stringify(this.document.proof)
-        : "";
+  ngOnInit(): void {
+    this.proofJson = this.document.proof
+      ? JSON.stringify(this.document.proof)
+      : "";
 
-      switch (this.type){
-        case 'VC':
-          if (Object.getPrototypeOf(this.document.credentialSubject) === Object.prototype) {
-            this.subjects.push(this.document.credentialSubject);
+    switch (this.type) {
+      case 'VC':
+        if (Object.getPrototypeOf(this.document.credentialSubject) === Object.prototype) {
+          this.subjects.push(this.document.credentialSubject);
+        }
+        else {
+          for (let i = 0; i < this.document.credentialSubject.length; i++) {
+            this.subjects.push(this.document.credentialSubject[i]);
           }
-          else {
-            for(let i=0;i<this.document.credentialSubject.length;i++)
-            {
-              this.subjects.push(this.document.credentialSubject[i]);
-            }
+        }
+        break;
+      case 'VP':
+        if (Object.getPrototypeOf(this.document.verifiableCredential) === Object.prototype) {
+          this.subjects.push(this.document.verifiableCredential);
+        }
+        else {
+          for (let i = 0; i < this.document.verifiableCredential.length; i++) {
+            this.subjects.push(this.document.verifiableCredential[i]);
           }
-          break;
-        case 'VP':
-          if (Object.getPrototypeOf(this.document.verifiableCredential) === Object.prototype) {
-            this.subjects.push(this.document.verifiableCredential);
-          }
-          else {
-            for(let i=0;i<this.document.verifiableCredential.length;i++)
-            {
-              this.subjects.push(this.document.verifiableCredential[i]);
-            }
-          }
-          break;
-      }
+        }
+        break;
     }
+  }
 
-    GetSchema(id: any) {
-        return this.schemas.filter((schema)=> schema.uuid === id)[0];
-    }
+  GetSchema(id: any): any {
+    return this.schemas.find((schema) => schema?.context?.type === id);
+  }
 }
