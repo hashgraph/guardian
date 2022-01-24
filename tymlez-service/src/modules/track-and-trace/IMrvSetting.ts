6bbe65f3bca2bb3a1e59410703343f63200dee68
Paul Li <engineforce@gmail.com>
2022-01-24 17:14:16 +1100

    Added correct units to MRV settings

--- tymlez-service/src/modules/track-and-trace/IMrvSetting.ts
+++ tymlez-service/src/modules/track-and-trace/IMrvSetting.ts
@@ -1,9 +1,11 @@
 import type { IIsoDate } from '@entity/IIsoDate';
+import type { IMetricTon } from '@entity/IMetricTon';
 import type { ITimeSpanMsec } from '@entity/ITimeSpanMsec';
+import type { kWh } from '@entity/kWh';
 
 export interface IMrvSetting {
-  mrvEnergyAmount: number;
-  mrvCarbonAmount: number;
+  mrvEnergyAmount: kWh;
+  mrvCarbonAmount: IMetricTon;
   mrvTimestamp: IIsoDate;
   mrvDuration: ITimeSpanMsec;
 }
