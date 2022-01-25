09fad90396c2d27760e3d0b43561e11d93579dd5
Stepan Kiryakov <stepan.kiryakov@envisionblockchain.com>
2022-01-25 16:50:56 +0400

    update filter block

--- ui-service/src/policy-engine/blocks/filters-addon-block.ts
+++ ui-service/src/policy-engine/blocks/filters-addon-block.ts
@@ -12,15 +12,6 @@ import { findOptions } from '@policy-engine/helpers/find-options';
     blockType: 'filtersAddon'
 })
 export class FiltersAddonBlock {
-    // field
-    // type: dropdown
-    // dataSource
-    // dataSource filters
-    // title
-    // name/value fields
-    // canBeEmpty: bool
-    // defaultValue
-
     private lastData: any;
     private lastValue: any;
 
@@ -40,15 +31,10 @@ export class FiltersAddonBlock {
             if (ref.options.type == 'dropdown') {
                 ref.filters[ref.options.field] = "";
             }
-
-            if (ref.options.type == 'unselected') {
-                ref.filters[ref.options.field] = ref.options.filterValue
-            }
         } else {
             this.lastData = null;
             this.lastValue = null;
         }
-
     }
 
     async getData(user: IAuthUser) {
@@ -63,51 +49,9 @@ export class FiltersAddonBlock {
             canBeEmpty: ref.options.canBeEmpty
         };
 
-        let data: any[];
+        let data: any[] = ref.getSources(userFull);
+        
         if (ref.options.type == 'dropdown') {
-            let filters: any = {};
-            if (ref.options.onlyOwnDocuments) {
-                filters.owner = userFull.did;
-            }
-            if (ref.options.onlyAssignDocuments) {
-                filters.assign = userFull.did;
-            }
-            if (ref.options.optionSourceSchema) {
-                filters.schema = ref.options.optionSourceSchema
-            }
-            if (ref.options.optionSourceType) {
-                filters.type = ref.options.optionSourceType;
-            }
-
-            switch (ref.options.optionSource) {
-                case 'vc-documents':
-                    filters.policyId = ref.policyId;
-                    console.log(filters);
-                    data = await this.guardians.getVcDocuments(filters);
-                    break;
-
-                case 'did-documents':
-                    data = await this.guardians.getDidDocuments(filters);
-                    break;
-
-                case 'vp-documents':
-                    filters.policyId = ref.policyId;
-                    data = await this.guardians.getVpDocuments(filters);
-                    break;
-
-                case 'approve':
-                    filters.policyId = ref.policyId;
-                    data = await this.guardians.getApproveDocuments(filters);
-                    break;
-
-                case 'source':
-                    data = [];
-                    break;
-
-                default:
-                    throw new BlockActionError(`dataType "${ref.options.dataType}" is unknown`, ref.blockType, ref.uuid)
-            }
-            data = data || [];
             this.lastData = data.map((e) => {
                 return {
                     name: findOptions(e, ref.options.optionName),
@@ -120,29 +64,25 @@ export class FiltersAddonBlock {
             block.filterValue = this.lastValue;
         }
 
-        if (ref.options.type == 'unselected') {
-            block.filterValue = ref.options.filterValue;
-        }
-
         return block;
     }
 
-    setData(user: IAuthUser, data: any) {
+    async setData(user: IAuthUser, data: any) {
         const ref = PolicyBlockHelpers.GetBlockRef(this);
         const filter: any = {};
         if (!data) {
-            throw new BlockActionError(`filter value "${ref.options.dataType}" is unknown`, ref.blockType, ref.uuid)
+            throw new BlockActionError(`filter value is unknown`, ref.blockType, ref.uuid)
         }
         if (ref.options.type == 'dropdown') {
             const value = data.filterValue;
             if (!this.lastData) {
-                throw new BlockActionError(`data "${ref.options.dataType}" is unknown`, ref.blockType, ref.uuid)
+                await this.getData(user);
             }
             const selectItem = this.lastData.find((e:any) => e.value == value);
             if (selectItem) {
                 filter[ref.options.field] = selectItem.value;
             } else if (!ref.options.canBeEmpty) {
-                throw new BlockActionError(`filter value "${ref.options.dataType}" is unknown`, ref.blockType, ref.uuid)
+                throw new BlockActionError(`filter value is unknown`, ref.blockType, ref.uuid)
             }
             this.lastValue = value;
         }
@@ -156,14 +96,11 @@ export class FiltersAddonBlock {
             resultsContainer.addBlockError(ref.uuid, 'Option "type" does not set');
         } else {
             switch (ref.options.type) {
-                case 'unselected':
-                    break;
-
                 case 'dropdown':
                     break;
 
                 default:
-                    resultsContainer.addBlockError(ref.uuid, 'Option "type" must be a "unselected|dropdown"');
+                    resultsContainer.addBlockError(ref.uuid, 'Option "type" must be a "dropdown"');
             }
         }
     }
