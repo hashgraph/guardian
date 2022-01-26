b8d8ffa89d1e39d3221b9118aae8836c6202d3c0
simvalery <valeriy.simonov@envisionblockchain.com>
2022-01-26 18:33:25 +0300

    fis kills
    Signed-off-by: simvalery <valeriy.simonov@envisionblockchain.com>

--- api-tests/index.js
+++ api-tests/index.js
@@ -1,25 +1,47 @@
-const { exec } = require('child_process');
+const { spawn } = require('child_process');
+const kill  = require('tree-kill');
 const path = require('path');
+
+const processes = []
+
+function sleep(time) {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve();
+        }, time)
+    })
+}
 
 describe('Tests', function() {
     before(async function() {
-        exec(`npm --prefix ${path.resolve(path.join('..', 'message-broker'))} start`, (error, stdout, stderr) => {
-            console.log(error, stdout, stderr);
-        });
-        exec(`npm --prefix ${path.resolve(path.join('..', 'guardian-service'))} start`, (error, stdout, stderr) => {
-            console.log(error, stdout, stderr);
-        })
-        exec(`npm --prefix ${path.resolve(path.join('..', 'ui-service'))} start`, (error, stdout, stderr) => {
-            console.log(error, stdout, stderr);
-        })
+        this.timeout(10000000000);
+        const pathArray = [
+            path.resolve(path.join('..', 'message-broker')),
+            path.resolve(path.join('..', 'guardian-service')),
+            path.resolve(path.join('..', 'ui-service'))
+        ];
+        for (let p of pathArray) {
+            processes.push(
+                spawn('npm start', {
+                    cwd: p,
+                    shell: true,
+                    // detached: true
+                })
+            )
+            await sleep(5000);
+            console.log('spawned ', p);
+        }
     })
 
     it('test', async function() {
         this.timeout(10000000000);
-        await new Promise(resolve => {
-            setTimeout(() => {
-                resolve()
-            }, 100000);
-        })
+        await sleep(1000);
+    })
+
+    after(async function() {
+        for (let proc of processes) {
+            kill(proc.pid);
+            console.log('killed ', proc)
+        }
     })
 });
