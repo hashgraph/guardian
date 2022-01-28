426a55c1936ceeef6cfc55b0ca24004d03fb1bb5
simvalery <valeriy.simonov@envisionblockchain.com>
2022-01-28 16:55:59 +0300

    fix configs
    Signed-off-by: simvalery <valeriy.simonov@envisionblockchain.com>

--- api-tests/index.js
+++ api-tests/index.js
@@ -1,6 +1,7 @@
 const { spawn } = require('child_process');
 const kill  = require('tree-kill');
 const path = require('path');
+const fs = require('fs');
 
 const processes = []
 
@@ -12,8 +13,19 @@ function sleep(time) {
     })
 }
 
+
 describe('Tests', function() {
     before(async function() {
+        const configs = [
+            {from: path.resolve(path.join('configs', 'guardian-service', 'config.json')) , to:path.resolve(path.join('..', 'guardian-service', 'config.json'))},
+            {from: path.resolve(path.join('configs', 'ui-service', '.env')) , to:path.resolve(path.join('..', 'ui-service', '.env'))},
+            {from: path.resolve(path.join('configs', 'ipfs-client', '.env')) , to:path.resolve(path.join('..', 'ipfs-client', '.env'))},
+        ]
+
+        for (let conf of configs) {
+            fs.copyFileSync(conf.from, conf.to);
+        }
+
         this.timeout(10000000000);
         const pathArray = [
             path.resolve(path.join('..', 'message-broker')),
@@ -29,7 +41,6 @@ describe('Tests', function() {
                 })
             )
             await sleep(5000);
-            console.log('spawned ', p);
         }
     })
 
@@ -41,7 +52,6 @@ describe('Tests', function() {
     after(async function() {
         for (let proc of processes) {
             kill(proc.pid);
-            console.log('killed ', proc)
         }
     })
 });
