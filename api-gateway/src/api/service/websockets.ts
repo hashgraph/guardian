5f9870871b52e8717e385fb1ecf21aa604e59c17
simvalery <valeriy.simonov@envisionblockchain.com>
2022-03-24 12:54:32 +0300

    method rename
    Signed-off-by: simvalery <valeriy.simonov@envisionblockchain.com>

--- api-gateway/src/api/service/websockets.ts
+++ api-gateway/src/api/service/websockets.ts
@@ -11,12 +11,12 @@ export class WebSocketsService {
         private channel: any
     ) {
         this.wss = new WebSocket.Server({server});
-        this.registerPingPongAnswers();
+        this.registerHeartbeatAnswers();
         this.registerAuthorisation();
         this.registerMessageHandler();
     }
 
-    private registerPingPongAnswers(): void {
+    private registerHeartbeatAnswers(): void {
         this.wss.on('connection', async (ws: any, req: IncomingMessage) => {
             ws.on('message', (data: Buffer) => {
                 switch (data.toString()) {
