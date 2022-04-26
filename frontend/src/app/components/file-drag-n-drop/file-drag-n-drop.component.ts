38bd9882a00871bad64a4410517d63a9a3cb9a3d
artembuslaev <buslaew.ar@mail.ru>
2022-04-26 16:40:38 +0400

    added drag n drop for import policies and schemas
    Signed-off-by: artembuslaev <buslaew.ar@mail.ru>

---
+++ frontend/src/app/components/file-drag-n-drop/file-drag-n-drop.component.ts
@@ -0,0 +1,63 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { NgxFileDropEntry } from 'ngx-file-drop';
+import { ToastrService } from 'ngx-toastr';
+
+@Component({
+  selector: 'app-file-drag-n-drop',
+  templateUrl: './file-drag-n-drop.component.html',
+  styleUrls: ['./file-drag-n-drop.component.css']
+})
+export class FileDragNDropComponent implements OnInit {
+
+  @Output() onFileLoaded: EventEmitter<any> = new EventEmitter();
+  @Input() dropZoneLabel: string = "";
+
+  constructor(
+    private toastr: ToastrService
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+  public droppedFile(files: NgxFileDropEntry[]) {
+    if (files.length > 1) {
+      this.toastr.error("Cannot add more than 1 files", "File import error", { positionClass: 'toast-bottom-right' })
+    } else {
+        const droppedFile = files[0];
+        if (droppedFile.fileEntry.isFile && this.isFileAllowed(droppedFile.fileEntry.name)) {
+          const fileEntry = droppedFile.fileEntry as any;
+          fileEntry.file((file: File) => {
+            this.onFileLoaded.emit(file);
+          });
+        } else {
+          this.toastr.error("Only files in '.zip' format are accepted", "File import error", { positionClass: 'toast-bottom-right' });
+        }
+    }
+  }
+
+  public importFromFile() {
+    const input = document.createElement('input');
+    input.type = 'file';
+    input.accept = '.zip';
+    input.click();
+    input.onchange = (e: any) => {
+      const file = e.target.files[0];
+      this.onFileLoaded.emit(file);
+    }
+  }
+
+  private isFileAllowed(fileName: string) {
+    let isFileAllowed = false;
+    const allowedFiles = ['.zip'];
+    const regex = /(?:\.([^.]+))?$/;
+    const extension = regex.exec(fileName);
+    if (extension) {
+      for (const ext of allowedFiles) {
+        if (ext === extension[0]) {
+          isFileAllowed = true;
+        }
+      }
+    }
+    return isFileAllowed;
+  }
+}
