7a4a447110e3d1eb80a45b41bca3e00c36049490
Niko Latkin <niko.latkin@envisionblockchain.com>
2022-04-29 15:59:17 -0500

    #806 #808 partially #807 and #774

--- e2e-tests/cypress/integration/api-tests/policies/getPoliciesBlocks.spec.js
+++ e2e-tests/cypress/integration/api-tests/policies/getPoliciesBlocks.spec.js
@@ -4,14 +4,14 @@ import { policyId } from "../../../fixtures/policy.json";
 context('Policies', () => {
     const authorization = Cypress.env('authorization');
 
-    it('check returns of the policy', () => {
-      const urlPoliciesId = {
+    it('check returns of the blocks', () => {
+      const url = {
         method: 'GET',
-        url: Cypress.env('api_server') + 'policies/' + policyId,
+        url: Cypress.env('api_server') + 'policies/' + policyId + '/blocks',
         headers: {
           authorization,
         }};
-      cy.request(urlPoliciesId)
+      cy.request(url)
           .should((response) => {
           expect(response.status).to.eq(200)
           expect(response.body).to.not.be.oneOf([null, ""])
