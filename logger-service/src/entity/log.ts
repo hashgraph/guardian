c3854accd7a328d8c6f6fc3fc92e59e7d6f77fe9
simvalery <valeriy.simonov@envisionblockchain.com>
2022-07-05 15:18:59 +0300

    logger-service linting
    Signed-off-by: simvalery <valeriy.simonov@envisionblockchain.com>

--- logger-service/src/entity/log.ts
+++ logger-service/src/entity/log.ts
@@ -1,20 +1,38 @@
 import { Column, CreateDateColumn, Entity, ObjectIdColumn } from 'typeorm';
 import { ILog, LogType } from '@guardian/interfaces';
 
+/**
+ * Log message
+ */
 @Entity()
 export class Log implements ILog {
+    /**
+     * Entity id
+     */
     @ObjectIdColumn()
     id: string;
 
+    /**
+     * Message
+     */
     @Column()
     message: string;
 
+    /**
+     * Type
+     */
     @Column()
     type: LogType;
 
+    /**
+     * Datetime
+     */
     @CreateDateColumn()
     datetime: Date;
 
+    /**
+     * Attributes
+     */
     @Column()
     attributes?: string[]
 }
