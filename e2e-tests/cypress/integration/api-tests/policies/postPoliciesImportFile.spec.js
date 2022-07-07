4ef7ba9633022d5420284e75c1e37285a0e30d3b
kirill-tolochko <kirill-tolochko@users.noreply.github.com>
2022-07-07 14:06:23 +0300

    #774 Tests for import policy by message and by file

---
+++ e2e-tests/cypress/integration/api-tests/policies/postPoliciesImportFile.spec.js
@@ -0,0 +1,60 @@
+/// <reference types="cypress" />
+
+import { policyId } from "../../../fixtures/policy.json";
+
+context('Policies', () => {
+  const authorization = Cypress.env('authorization');
+
+  before(() => {
+    const url = {
+      method: 'GET',
+      url: Cypress.env('api_server') + 'policies/' + policyId + '/export/file',
+      encoding: null,
+      headers: {
+        authorization
+      }
+    }
+    cy.request(url).then((response) => {
+      let policy = Cypress.Blob.arrayBufferToBinaryString(response.body)
+      cy.writeFile('cypress/fixtures/exportedPolicy.policy', policy, 'binary')
+    })
+  })
+
+
+  it('check returns of all policies', () => {
+    cy.fixture('exportedPolicy.policy', 'binary').then((binary) => Cypress.Blob.binaryStringToBlob(binary))
+      .then((file) => {
+        cy.request({
+          method: 'POST',
+          url: `${Cypress.env('api_server')}policies/import/file`,
+          body: file,
+          headers: {
+            'content-type': 'binary/octet-stream',
+            authorization
+          },
+          timeout: 180000
+        })
+          .then(response => {
+            let responseTextJSon = JSON.parse(Cypress.Blob.arrayBufferToBinaryString(response.body))
+            let firstPolicyId = responseTextJSon.at(-1).id
+            let firstPolicyStatus = responseTextJSon.at(-1).status
+            expect(firstPolicyStatus).to.equal('DRAFT')
+            cy.request({
+              method: 'PUT',
+              url: Cypress.env('api_server') + 'policies/' + firstPolicyId + '/publish',
+              body: { policyVersion: "1.2.5" },
+              headers: { authorization },
+              timeout: 600000
+            })
+              .should((response) => {
+                let secondPolicyId = response.body.policies.at(-1).id
+                let policyStatus = response.body.policies.at(-1).status
+                expect(response.status).to.eq(200)
+                expect(response.body).to.not.be.oneOf([null, ""])
+                expect(firstPolicyId).to.equal(secondPolicyId)
+                expect(policyStatus).to.equal('PUBLISH')
+              })
+          })
+      })
+  })
+})
--- e2e-tests/cypress/integration/api-tests/policies/postPoliciesImportMessage.spec.js
+++ e2e-tests/cypress/integration/api-tests/policies/postPoliciesImportMessage.spec.js
@@ -1,11 +1,37 @@
 /// <reference types="cypress" />
 
 context('Policy - Import', () => {
-    it('imports new policy and all associated artifacts from IPFS into the local DB', () => {
-        cy.request('POST', (Cypress.env('api_server') + 'policies/import/message'), {
-            "messageId": Cypress.env('irec_policy')
-        }).should((response) => {
-            expect(response.status).to.eq(201)
-        })
+  const authorization = Cypress.env('authorization');
+
+  it('imports new policy and all associated artifacts from IPFS into the local DB', () => {
+    cy.request({
+      method: 'POST',
+      url: `${Cypress.env('api_server')}policies/import/message`,
+      body: { messageId: (Cypress.env('irec_policy')) },
+      headers: {
+        authorization,
+      },
+      timeout: 180000
     })
+      .then(response => {
+        let firstPolicyId = response.body.at(-1).id
+        let firstPolicyStatus = response.body.at(-1).status
+        expect(firstPolicyStatus).to.equal('DRAFT')
+        cy.request({
+          method: 'PUT',
+          url: Cypress.env('api_server') + 'policies/' + firstPolicyId + '/publish',
+          body: { policyVersion: "1.2.5" },
+          headers: { authorization },
+          timeout: 600000
+        })
+          .should((response) => {
+            let secondPolicyId = response.body.policies.at(-1).id
+            let policyStatus = response.body.policies.at(-1).status
+            expect(response.status).to.eq(200)
+            expect(response.body).to.not.be.oneOf([null, ""])
+            expect(firstPolicyId).to.equal(secondPolicyId)
+            expect(policyStatus).to.equal('PUBLISH')
+          })
+      })
+  })
 })
