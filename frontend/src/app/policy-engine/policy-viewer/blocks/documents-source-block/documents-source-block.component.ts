03692650ebb01c4dd43f47ba0a74b06e30eb3200
Stepan Kiryakov <stepan.kiryakov@envisionblockchain.com>
2022-08-04 18:10:36 +0400

    fix merge
    Signed-off-by: Stepan Kiryakov <stepan.kiryakov@envisionblockchain.com>

--- frontend/src/app/policy-engine/policy-viewer/blocks/documents-source-block/documents-source-block.component.ts
+++ frontend/src/app/policy-engine/policy-viewer/blocks/documents-source-block/documents-source-block.component.ts
@@ -102,7 +102,7 @@ export class DocumentsSourceBlockComponent implements OnInit {
     }
 
     async setData(data: any) {
-        if (data?.data?.length > 0) {
+        if (data) {
             const fields: any[] = data.fields || [];
             this.fieldMap = {};
             this.fields = [];
