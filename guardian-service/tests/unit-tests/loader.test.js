c31207e46778c1552b8912373dee78869efa7206
Stepan Kiryakov <stepan.kiryakov@envisionblockchain.com>
2022-08-05 19:52:31 +0400

    fix Aliases
    Signed-off-by: Stepan Kiryakov <stepan.kiryakov@envisionblockchain.com>

--- guardian-service/tests/unit-tests/loader.test.js
+++ guardian-service/tests/unit-tests/loader.test.js
@@ -8,7 +8,8 @@ moduleAlias.addAliases({
   "@auth": process.cwd() + '/dist' + "/auth",
   "@policy-engine": process.cwd() + '/dist' + "/policy-engine",
   "@hedera-modules": process.cwd() + '/dist' + "/hedera-modules/index",
-  "@document-loader": process.cwd() + '/dist' + "/document-loader"
+  "@document-loader": process.cwd() + '/dist' + "/document-loader",
+  "@database-modules": process.cwd() + '/dist' + "/database-modules"
 });
 
 const { expect, assert } = require('chai');
