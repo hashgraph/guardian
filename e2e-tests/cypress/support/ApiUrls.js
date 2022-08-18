8c7fb353b79065343458989771c0c6d0989f7e03
leobart <leobartleo@gmail.com>
2022-08-18 13:47:00 +0100

    1148-improve framework,logs tests

--- e2e-tests/cypress/support/ApiUrls.js
+++ e2e-tests/cypress/support/ApiUrls.js
@@ -4,6 +4,9 @@ ApiServer: "http://localhost:3000/api/v1/",
 Accounts: "http://localhost:3000/api/v1/accounts/",
 RootAuthorities: "http://localhost:3000/api/v1/accounts/root-authorities",
 //Tokens
-ListOfTokens: "http://localhost:3000/api/v1/tokens",
+ListOfTokens: "http://localhost:3000/api/v1/tokens/",
+//Logs
+Logs: "http://localhost:3000/api/v1/logs",
+LogsAttributes: "http://localhost:3000/api/v1/logs/attributes/",
 };
 export default API;
