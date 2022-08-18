3bf3654181fc6ac400dfcec30bd454e620d0f0cd
leobart <leobartleo@gmail.com>
2022-08-18 13:47:00 +0100

    1148-improve framework,logs tests
    Signed-off-by: Leo Bart <leobartleo@gmail.com>

--- e2e-tests/cypress/integration/api-tests/logs/getAttributes.spec.js
+++ e2e-tests/cypress/integration/api-tests/logs/getAttributes.spec.js
@@ -3,14 +3,11 @@ import { METHOD, STATUS_CODE } from "../../../support/api/api-const";
 import API from "../../../support/ApiUrls";
 
 
-context('Tokens', () => {
+context('Logs', () => {
   const authorization = Cypress.env('authorization');
-  const tokenId = Cypress.env('tokenId');
-  const user = Cypress.env('root_user');
 
-  it('get all tokens', () => {
-    cy.sendRequest(METHOD.GET, API.ListOfTokens + tokenId + '/' + user  + '/info', {authorization,}).then((resp) => {
-
+  it('get logs attributes as a StandardRegistry', () => {
+    cy.sendRequest(METHOD.GET, API.LogsAttributes, {authorization,}).then((resp) => {
       expect(resp.status).eql(STATUS_CODE.OK)
       expect(resp.body).to.not.be.oneOf([null, ""])
     })
