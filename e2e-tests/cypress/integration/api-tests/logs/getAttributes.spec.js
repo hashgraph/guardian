2c886a6592a73702e593e4f3cf36520696ffdd1c
leobart <leobartleo@gmail.com>
2022-08-19 11:48:24 +0100

    tokens
    Signed-off-by: Leo Bart <leobartleo@gmail.com>

--- e2e-tests/cypress/integration/api-tests/logs/getAttributes.spec.js
+++ e2e-tests/cypress/integration/api-tests/logs/getAttributes.spec.js
@@ -2,14 +2,15 @@
 import { METHOD, STATUS_CODE } from "../../../support/api/api-const";
 import API from "../../../support/ApiUrls";
 
+context("Logs", () => {
+    const authorization = Cypress.env("authorization");
 
-context('Logs', () => {
-  const authorization = Cypress.env('authorization');
-
-  it('get logs attributes as a StandardRegistry', () => {
-    cy.sendRequest(METHOD.GET, API.LogsAttributes, {authorization,}).then((resp) => {
-      expect(resp.status).eql(STATUS_CODE.OK)
-      expect(resp.body).to.not.be.oneOf([null, ""])
-    })
-})
-})
+    it("get logs attributes as a StandardRegistry", () => {
+        cy.sendRequest(METHOD.GET, API.LogsAttributes, { authorization }).then(
+            (resp) => {
+                expect(resp.status).eql(STATUS_CODE.OK);
+                expect(resp.body).to.not.be.oneOf([null, ""]);
+            }
+        );
+    });
+});
