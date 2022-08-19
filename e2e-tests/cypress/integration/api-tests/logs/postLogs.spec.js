d4e957e13db05772eeb936045c5ac370046764c5
leobart <leobartleo@gmail.com>
2022-08-19 11:48:24 +0100

    tokens
    Signed-off-by: Leo Bart <leobartleo@gmail.com>

--- e2e-tests/cypress/integration/api-tests/logs/postLogs.spec.js
+++ e2e-tests/cypress/integration/api-tests/logs/postLogs.spec.js
@@ -2,14 +2,15 @@
 import { METHOD, STATUS_CODE } from "../../../support/api/api-const";
 import API from "../../../support/ApiUrls";
 
+context("Logs", () => {
+    const authorization = Cypress.env("authorization");
 
-context('Logs', () => {
-  const authorization = Cypress.env('authorization');
-
-  it('post request all logs as a StandardRegistry', () => {
-    cy.sendRequest(METHOD.POST, API.Logs, {authorization,}).then((resp) => {
-      expect(resp.status).eql(STATUS_CODE.OK)
-      expect(resp.body).to.not.be.oneOf([null, ""])
-    })
-})
-})
+    it("post request all logs as a StandardRegistry", () => {
+        cy.sendRequest(METHOD.POST, API.Logs, { authorization }).then(
+            (resp) => {
+                expect(resp.status).eql(STATUS_CODE.OK);
+                expect(resp.body).to.not.be.oneOf([null, ""]);
+            }
+        );
+    });
+});
--- e2e-tests/cypress/integration/api-tests/tokens/getUserInfoSelectedToken.spec.js
+++ e2e-tests/cypress/integration/api-tests/tokens/getUserInfoSelectedToken.spec.js
@@ -2,17 +2,20 @@
 import { METHOD, STATUS_CODE } from "../../../support/api/api-const";
 import API from "../../../support/ApiUrls";
 
+context("Tokens", () => {
+    const authorization = Cypress.env("authorization");
+    const tokenId = Cypress.env("tokenId");
+    const user = Cypress.env("root_user");
 
-context('Tokens', () => {
-  const authorization = Cypress.env('authorization');
-  const tokenId = Cypress.env('tokenId');
-  const user = Cypress.env('root_user');
+    it("get token info", () => {
+        cy.sendRequest(
+            METHOD.GET,
+            API.ListOfTokens + tokenId + "/" + user + "/info",
+            { authorization }
+        ).then((resp) => {
+            expect(resp.status).eql(STATUS_CODE.OK);
+            expect(resp.body).to.not.be.oneOf([null, ""]);
+        });
+    });
+});
 
-  it('get all tokens', () => {
-    cy.sendRequest(METHOD.GET, API.ListOfTokens + tokenId + '/' + user  + '/info', {authorization,}).then((resp) => {
-
-      expect(resp.status).eql(STATUS_CODE.OK)
-      expect(resp.body).to.not.be.oneOf([null, ""])
-    })
-})
-})
