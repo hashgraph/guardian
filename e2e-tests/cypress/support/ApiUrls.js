8bd55b7bfaa24768ad55580d4f5c23d12e99e7e7
Leo Bart <leobartleo@gmail.com>
2022-08-19 23:37:47 +0100

    update

--- e2e-tests/cypress/support/ApiUrls.js
+++ e2e-tests/cypress/support/ApiUrls.js
@@ -1,12 +1,12 @@
 const API = {
-ApiServer: "http://localhost:3000/api/v1/",
-//Accounts
-Accounts: "http://localhost:3000/api/v1/accounts/",
-RootAuthorities: "http://localhost:3000/api/v1/accounts/root-authorities",
-//Tokens
-ListOfTokens: "http://localhost:3000/api/v1/tokens/",
-//Logs
-Logs: "http://localhost:3000/api/v1/logs",
-LogsAttributes: "http://localhost:3000/api/v1/logs/attributes/",
+    ApiServer: "http://localhost:3000/api/v1/",
+    //Accounts
+    Accounts: "http://localhost:3000/api/v1/accounts/",
+    RootAuthorities: "http://localhost:3000/api/v1/accounts/root-authorities",
+    //Tokens
+    ListOfTokens: "http://localhost:3000/api/v1/tokens/",
+    //Logs
+    Logs: "http://localhost:3000/api/v1/logs",
+    LogsAttributes: "http://localhost:3000/api/v1/logs/attributes/",
 };
 export default API;
