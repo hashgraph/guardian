c104262732c08ce2363649e323fe49b3abdd5e17
Bartleonid <bartleonid@hotmail.com>
2022-09-20 12:13:51 +0100

    ipfs and external (#1322)

--- e2e-tests/cypress/support/ApiUrls.js
+++ e2e-tests/cypress/support/ApiUrls.js
@@ -1,30 +1,37 @@
 const API = {
-ApiServer: "http://localhost:3000/api/v1/",
-//Accounts
-Accounts: "http://localhost:3000/api/v1/accounts/",
-AccountsLogin: "http://localhost:3000/api/v1/accounts/login/",
-RootAuthorities: "http://localhost:3000/api/v1/accounts/root-authorities",
-Installer: "http://localhost:3000/api/v1/accounts/installer",
-StandartRegistries: "http://localhost:3000/api/v1/accounts/standard-registries",
-Balance: "http://localhost:3000/api/v1/accounts/balance",
-//Profiles
-Profiles: "http://localhost:3000/api/v1/profiles/",
-//Tokens
-ListOfTokens: "http://localhost:3000/api/v1/tokens/",
-//Logs
-Logs: "http://localhost:3000/api/v1/logs",
-LogsAttributes: "http://localhost:3000/api/v1/logs/attributes/",
-//Schemas
-Schemas:"http://localhost:3000/api/v1/schemas/",
-SchemasSystem:"http://localhost:3000/api/v1/schemas/system/",
-SchemasSystemEntity:"http://localhost:3000/api/v1/schemas/system/entity/",
-SchemaImportMsgPreview: "http://localhost:3000/api/v1/schemas/import/message/preview",
-SchemaImportFilePreview: "http://localhost:3000/api/v1/schemas/import/file/preview",
-SchemaImport: "http://localhost:3000/api/v1/schemas/import/",
-SchemasType:"http://localhost:3000/api/v1/schemas/type",
-//TrustChains
-Trustchains:"http://localhost:3000/api/v1/trustchains/",
-//Settings
-SettingsEnv:"http://localhost:3000/api/v1/settings/environment",
+    ApiServer: "http://localhost:3000/api/v1/",
+    //Accounts
+    Accounts: "http://localhost:3000/api/v1/accounts/",
+    AccountsLogin: "http://localhost:3000/api/v1/accounts/login/",
+    RootAuthorities: "http://localhost:3000/api/v1/accounts/root-authorities",
+    Installer: "http://localhost:3000/api/v1/accounts/installer",
+    StandartRegistries:
+        "http://localhost:3000/api/v1/accounts/standard-registries",
+    Balance: "http://localhost:3000/api/v1/accounts/balance",
+    //Profiles
+    Profiles: "http://localhost:3000/api/v1/profiles/",
+    //Tokens
+    ListOfTokens: "http://localhost:3000/api/v1/tokens/",
+    //Logs
+    Logs: "http://localhost:3000/api/v1/logs",
+    LogsAttributes: "http://localhost:3000/api/v1/logs/attributes/",
+    //Schemas
+    Schemas: "http://localhost:3000/api/v1/schemas/",
+    SchemasSystem: "http://localhost:3000/api/v1/schemas/system/",
+    SchemasSystemEntity: "http://localhost:3000/api/v1/schemas/system/entity/",
+    SchemaImportMsgPreview:
+        "http://localhost:3000/api/v1/schemas/import/message/preview",
+    SchemaImportFilePreview:
+        "http://localhost:3000/api/v1/schemas/import/file/preview",
+    SchemaImport: "http://localhost:3000/api/v1/schemas/import/",
+    SchemasType: "http://localhost:3000/api/v1/schemas/type",
+    //TrustChains
+    Trustchains: "http://localhost:3000/api/v1/trustchains/",
+    //Settings
+    SettingsEnv: "http://localhost:3000/api/v1/settings/environment",
+    //External
+    External: "http://localhost:3000/api/v1/external",
+    //ipfs
+    IPFSFile: "http://localhost:3000/api/v1/ipfs/file",
 };
 export default API;
