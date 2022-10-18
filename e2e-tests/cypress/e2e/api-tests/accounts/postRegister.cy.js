32abbcc4af02d23d048b5b8bf7a978df968b22e3
Bartleonid <bartleonid@hotmail.com>
2022-10-18 14:12:05 +0100

    negative accounts register (#1420)

--- e2e-tests/cypress/e2e/api-tests/accounts/postRegister.cy.js
+++ e2e-tests/cypress/e2e/api-tests/accounts/postRegister.cy.js
@@ -1,25 +1,154 @@
-context('Accounts', { tags: '@accounts' }, () => {
+import { METHOD, STATUS_CODE } from "../../../support/api/api-const";
+import API from "../../../support/ApiUrls";
 
-      it('register a new user and login with it',  () => {
-          const name = (Math.floor(Math.random() * 999) + 'test001')
-        cy.request('POST', (Cypress.env('api_server') + 'accounts/register'), {
+
+context("Accounts", { tags: "@accounts" }, () => {
+    it("register a new user and login with it", () => {
+        const name = Math.floor(Math.random() * 999) + "test001";
+        cy.request("POST", API.ApiServer + "accounts/register", {
             username: name,
-            password: 'test'
-        }).should((response) => {
-            expect(response.status).to.eq(201)
-            expect(response.body).to.have.property('username', name)
-            expect(response.body).to.have.property('did', null)
-            expect(response.body).to.have.property('role', 'USER')
-            expect(response.body).to.have.property('id')
-        }).then(()=>{
-            cy.request('POST', (Cypress.env('api_server') + 'accounts/login'), {
-                username: name,
-                password: 'test'
-            }).should((response) => {
-                expect(response.status).to.eq(200)
-                expect(response.body).to.have.property('username', name)
-                expect(response.body).to.have.property('role', 'USER')
-            })
+            password: "test",
         })
-    })
-})
+            .should((response) => {
+                expect(response.status).to.eq(201);
+                expect(response.body).to.have.property("username", name);
+                expect(response.body).to.have.property("did", null);
+                expect(response.body).to.have.property("role", "USER");
+                expect(response.body).to.have.property("id");
+            })
+            .then(() => {
+                cy.request(
+                    "POST",
+                    API.ApiServer + "accounts/login",
+                    {
+                        username: name,
+                        password: "test",
+                    }
+                ).should((response) => {
+                    expect(response.status).to.eq(200);
+                    expect(response.body).to.have.property("username", name);
+                    expect(response.body).to.have.property("role", "USER");
+                });
+            });
+    });
+
+    //Negative
+
+    it("should attempt to register a user with no body", () => {
+        cy.request({
+            method: METHOD.POST,
+            url: API.ApiServer + API.AccountRegister,
+            headers: {
+            },
+            failOnStatusCode:false,
+        }).then((resp) => {
+            expect(resp.status).eql(STATUS_CODE.ERROR);
+        });
+    });
+
+
+    it('should attempt to register a user with missing fields - username', () => {
+        cy.request({
+            method: METHOD.POST,
+            url: API.ApiServer + API.AccountRegister,
+            headers: {
+                password: "test",
+            },
+            failOnStatusCode:false,
+        }).then(resp => {
+            expect(resp.status).eql(STATUS_CODE.ERROR);
+        });
+    });
+
+    it('should attempt to register a user with missing fields - password', () => {
+        const name = Math.floor(Math.random() * 999) + "test001";
+        cy.request({
+            method: METHOD.POST,
+            url: API.ApiServer + API.AccountRegister,
+            headers: {
+                username: name,
+            },
+            failOnStatusCode:false,
+        }).then(resp => {
+            expect(resp.status).eql(STATUS_CODE.ERROR);
+        });
+    });
+
+
+    it('should attempt to register a user with invalid type', () => {
+        cy.request({
+            method: METHOD.POST,
+            url: API.ApiServer + API.AccountRegister,
+            headers: {
+                username: true,
+                password: "test",
+            },
+            failOnStatusCode:false,
+        }).then(response => {
+            expect(response.status).eql(STATUS_CODE.ERROR);
+        });
+    });
+
+    it('should attempt to create a user with invalid input', () => {
+        const name = Math.floor(Math.random() * 999) + "test001";
+        cy.request({
+            method: METHOD.POST,
+            url: API.ApiServer + API.AccountRegister,
+            headers: {
+                username: name,
+                name: "test",
+            },
+            failOnStatusCode:false,
+        }).then(response => {
+            expect(response.status).eql(STATUS_CODE.ERROR);
+        });
+    });
+
+    it('should attempt to register a user with invalid request method', () => {
+        const name = Math.floor(Math.random() * 999) + "test001";
+        cy.request({
+            method: METHOD.PUT,
+            url: API.ApiServer + API.AccountRegister,
+            headers: {
+                username: name,
+                password: "test",
+            },
+            failOnStatusCode:false,
+        }).then(response => {
+            expect(response.status).eql(STATUS_CODE.NOT_FOUND);
+        });
+    });
+
+    it('should attempt to register a user with invalid endpoint', () => {
+        const name = Math.floor(Math.random() * 999) + "test001";
+        cy.request({
+            method: METHOD.POST,
+            url: API.ApiServer + API.AccountRegister + "test",
+            headers: {
+                username: name,
+                password: "test",
+            },
+            failOnStatusCode:false,
+        }).then(response => {
+            expect(response.status).eql(STATUS_CODE.NOT_FOUND);
+        });
+    });
+
+
+    it('should attempt to register a user with extra data', () => {
+        const name = Math.floor(Math.random() * 999) + "test001";
+        cy.request({
+            method: METHOD.POST,
+            url: API.ApiServer + API.AccountRegister,
+            headers: {
+                username: name,
+                password: "test",
+                status: "Draft",
+            },
+            failOnStatusCode:false,
+        }).then(response => {
+            expect(response.status).eql(STATUS_CODE.ERROR);
+        });
+    });
+
+});
