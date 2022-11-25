13305061ef4e717307ad2c1866c7450a868ea897
simvalery <valeriy.simonov@envisionblockchain.com>
2022-11-25 14:46:33 +0400

    implement local ipfs node remove ipfs client
    Signed-off-by: simvalery <valeriy.simonov@envisionblockchain.com>

--- worker-service/src/api/ipfs-client.ts
+++ worker-service/src/api/ipfs-client.ts
@@ -1,19 +1,81 @@
 import { Web3Storage } from 'web3.storage';
 import Blob from 'cross-blob';
 import axios from 'axios';
+import { create } from 'ipfs-client'
 
+/**
+ * Providers type
+ */
+type IpfsProvider = 'web3storage' | 'local';
 /**
  * IPFS Client helper
  */
 export class IpfsClient {
+
+    /**
+     * IPFS provider
+     * @private
+     */
+    private readonly IPFS_PROVIDER: IpfsProvider = process.env.IPFS_PROVIDER as IpfsProvider || 'web3storage'
+
+    /**
+     * IPFS public gateway
+     * @private
+     */
+    private readonly IPFS_PUBLIC_GATEWAY = process.env.IPFS_PUBLIC_GATEWAY || 'https://ipfs.io/ipfs/${cid}';
+
     /**
      * Web3storage instance
      * @private
      */
-    private readonly client: Web3Storage;
+    private readonly client: Web3Storage | any;
 
-    constructor(token: string) {
-        this.client = new Web3Storage({ token } as any)
+    /**
+     * Client options
+     * @private
+     */
+    private readonly options: {[key: string]: any} = {};
+
+    constructor(token?: string) {
+        this.options.token = token;
+        this.options.nodeAddress = process.env.IPFS_NODE_ADDRESS;
+
+        this.client = this.createClient();
+    }
+
+    /**
+     * Create ipfs client
+     * @private
+     */
+    private createClient(): Web3Storage | unknown {
+        let client;
+
+        switch (this.IPFS_PROVIDER) {
+            case 'web3storage': {
+                if (!this.options.token) {
+                    throw new Error('Web3Storage token is not set')
+                }
+                client = new Web3Storage({ token: this.options.token } as any);
+
+                break;
+            }
+
+            case 'local': {
+                if (!this.options.nodeAddress) {
+                    throw new Error('IPFS_NODE_ADDRESS variable is not set');
+                }
+                client = create({
+                    http: this.options.nodeAddress
+                });
+
+                break;
+            }
+
+            default:
+                throw new Error(`${this.IPFS_PROVIDER} provider is unknown`);
+        }
+
+        return client;
     }
 
     /**
@@ -22,7 +84,23 @@ export class IpfsClient {
      * @param beforeCallback
      */
     public async addFile(file: Blob): Promise<string> {
-        const cid = await this.client.put([file] as any, { wrapWithDirectory: false });
+        let cid;
+        switch (this.IPFS_PROVIDER) {
+            case 'web3storage': {
+                cid = await this.client.put([file] as any, { wrapWithDirectory: false });
+                break;
+            }
+
+            case 'local': {
+                const { path } = await this.client.add(await file.arrayBuffer());
+                cid = path;
+                break;
+            }
+
+            default:
+                throw new Error(`${this.IPFS_PROVIDER} provider is unknown`);
+        }
+        console.log(cid);
         return cid;
     }
 
@@ -32,7 +110,7 @@ export class IpfsClient {
      */
     public async getFile(cid: string): Promise<any> {
         const fileRes = await axios.get(
-            process.env.IPFS_PUBLIC_GATEWAY?.replace('${cid}', cid),
+            this.IPFS_PUBLIC_GATEWAY?.replace('${cid}', cid),
             {
                 responseType: 'arraybuffer',
                 timeout:
