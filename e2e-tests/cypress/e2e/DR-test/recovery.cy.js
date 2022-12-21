8837bbedccefcf34f45b34a778a7d8e6375bc350
Bartleonid <bartleonid@hotmail.com>
2022-12-21 19:45:09 +0000

    362 (#1600)
    * add ui tests readme
    
    * add ui tests dry-run
    
    * recovery data

---
+++ e2e-tests/cypress/e2e/DR-test/recovery.cy.js
@@ -0,0 +1,104 @@
+
+const Locators = {
+    createNew: "Create new",
+    srBtn: "Standard Registry",
+    submitBtn: '[type="submit"]',
+    restoreDataBtn: "Restore data",
+    refreshBtn: "refresh",
+    username: '[formcontrolname="login"]',
+    nextBtn: "Next",
+    connectBtn: "Connect",
+};
+
+describe("Disaster Recovery Testing", { tags: "@recovery" }, () => {
+    const name = Math.floor(Math.random() * 999) + "testName";
+    it("checks ", () => {
+        cy.viewport(1440, 900);
+
+        cy.visit("http://localhost:3000/");
+
+        //create new
+        cy.get("div > form > a")
+            .should("have.attr", "href")
+            .and("include", "/register")
+            .then(() => {
+                cy.contains(Locators.createNew).click();
+                cy.contains(Locators.srBtn).click();
+
+                const inputName = cy.get(Locators.username);
+                inputName.type(name);
+
+                cy.get(Locators.submitBtn).click();
+
+                cy.contains("Generate").click();
+                cy.wait(5000);
+
+                cy.get('[formcontrolname="hederaAccountId"]')
+                    .invoke("val")
+                    .then((hederaAccountId) => {
+                        cy.log(hederaAccountId);
+                        let accountId = hederaAccountId;
+
+                        cy.get('[formcontrolname="hederaAccountKey"]')
+                            .invoke("val")
+                            .then((hederaAccountKey) => {
+                                cy.log(hederaAccountKey);
+                                let accountKey = hederaAccountKey;
+
+                                cy.contains(Locators.nextBtn).click();
+                                cy.contains(Locators.connectBtn).click();
+                                cy.wait(16000);
+
+                                const standartRegistry = cy.contains(name);
+                                standartRegistry.click({ force: true });
+                                cy.contains("Log out").click({ force: true });
+
+                                cy.get("div > form > a")
+                                    .should("have.attr", "href")
+                                    .and("include", "/register")
+                                    .then(() => {
+                                        cy.contains(Locators.createNew).click();
+                                        cy.contains(Locators.srBtn).click();
+
+                                        const inputName = cy.get(
+                                            Locators.username
+                                        );
+                                        inputName.type(name);
+
+                                        cy.get(Locators.submitBtn).click();
+
+                                        const inputhederaAccountId = cy.get(
+                                            '[formcontrolname="hederaAccountId"]'
+                                        );
+                                        inputhederaAccountId.type(accountId);
+
+                                        const inputhederaAccountKey = cy.get(
+                                            '[formcontrolname="hederaAccountKey"]'
+                                        );
+                                        inputhederaAccountKey.type(accountKey);
+
+                                        cy.contains(
+                                            "*[role='img']",
+                                            "arrow_drop_down"
+                                        )
+                                            .trigger("mouseover")
+                                            .click();
+                                        cy.contains(
+                                            Locators.restoreDataBtn
+                                        ).click();
+                                        cy.wait(20000);
+                                        cy.contains(
+                                            Locators.refreshBtn
+                                        ).click();
+                                        cy.wait(120000);
+                                        cy.contains(
+                                            Locators.restoreDataBtn
+                                        ).click();
+                                    });
+                            });
+                    });
+            });
+    });
+});
+
+export {};
