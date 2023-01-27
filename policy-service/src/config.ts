88d5c3b871c5c357244206e1ec62be4b057e7eb6
simvalery <valeriy.simonov@envisionblockchain.com>
2023-01-27 12:47:24 +0400

    update transmute
    Signed-off-by: simvalery <valeriy.simonov@envisionblockchain.com>

--- policy-service/src/config.ts
+++ policy-service/src/config.ts
@@ -5,9 +5,7 @@ import 'reflect-metadata';
 moduleAlias.addAliases({
     '@api': __dirname + '/api',
     '@entity': __dirname + '/entity',
-    '@subscribers': __dirname + 'dist/subscribers',
     '@helpers': __dirname + '/helpers',
-    '@auth': __dirname + '/auth',
     '@policy-engine': __dirname + '/policy-engine',
     '@hedera-modules': __dirname + '/hedera-modules/index',
     '@database-modules': __dirname + '/database-modules/index',
