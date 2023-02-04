72074cfb6bfff45e375565576dbe3c13a30688b5
hnamzian <namazian87@gmail.com>
2023-02-04 12:38:17 +0300

    Created Root CA and Intermediate CA profiles

--- vault/hashicorp/scripts/keygen/keygen_cfssl.sh
+++ vault/hashicorp/scripts/keygen/keygen_cfssl.sh
@@ -5,6 +5,10 @@ source $PWD/vault/hashicorp/.env
 
 BASE_DIR=$PWD/vault/hashicorp
 CFSSL_CONFIG_DIR=$BASE_DIR/configs/cfssl
+CFSSL_PROFILES_DIR=$BASE_DIR/configs/cfssl/profiles
+CFSSL_PROFILES_CA_DIR=$CFSSL_PROFILES_DIR/ca
 
 CFSSL_CONFIG_FILE=$CFSSL_CONFIG_DIR/cfssl.json
+CFSSL_PROFILE_PATH_ROOT_CA=$CFSSL_PROFILES_CA_DIR/root.json
+CFSSL_PROFILE_PATH_INT_CA=$CFSSL_PROFILES_CA_DIR/intermediate_ca.json
 
