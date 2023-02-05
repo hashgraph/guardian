f931edbaea84671e7813a2b384677ac12ddb663b
hnamzian <namazian87@gmail.com>
2023-02-05 20:47:32 +0300

    Get/Set Wallet using Vault Secret Mnager directly instead of requesting from Auth Service

--- policy-service/src/helpers/wallet.ts
+++ policy-service/src/helpers/wallet.ts
@@ -1,6 +1,8 @@
 import { Singleton } from '@helpers/decorators/singleton';
 import { ServiceRequestsBase } from '@helpers/service-requests-base';
-import { WalletEvents, IGetKeyResponse } from '@guardian/interfaces';
+// import { WalletEvents, IGetKeyResponse } from '@guardian/interfaces';
+import { Wallet as WalletManager} from '@guardian/common'
+import { Users } from './users';
 
 /**
  * Key type
@@ -35,8 +37,18 @@ export class Wallet extends ServiceRequestsBase {
      * @param key
      */
     public async getKey(token: string, type: KeyType, key: string): Promise<string> {
-        const wallet = await this.request<IGetKeyResponse>(WalletEvents.GET_KEY, { token, type, key });
-        return wallet.key;
+        /**
+         * This block sends WalletEvents.GET_KEY to Auth Service to get Wallet PrivateKey in response
+         * 
+            const wallet = await this.request<IGetKeyResponse>(WalletEvents.GET_KEY, { token, type, key });
+            return wallet.key;
+         */
+
+        /**
+         * This block gets Wallet PrivateKey from Vault directly
+         */
+        const wallet = new WalletManager();
+        return await wallet.getKey(token, type, key);
     }
 
     /**
@@ -47,7 +59,18 @@ export class Wallet extends ServiceRequestsBase {
      * @param value
      */
     public async setKey(token: string, type: string, key: string, value: string) {
-        await this.request<any>(WalletEvents.SET_KEY, { token, type, key, value });
+        /**
+         * This block create/update a wallet by sending wallet data to Auth Service
+         * through WalletEvents.SET_KEY message
+         * 
+            await this.request<any>(WalletEvents.SET_KEY, { token, type, key, value });
+         */
+
+        /**
+         * This block creates/updates a Wallet by writing to Vault directly
+         */
+        const wallet = new WalletManager();
+        await wallet.setKey(token, type, key, value);
     }
 
     /**
@@ -56,8 +79,22 @@ export class Wallet extends ServiceRequestsBase {
      * @param key
      */
      public async getUserKey(did: string, type: KeyType, key: string): Promise<any> {
-        const wallet = await this.request<any>(WalletEvents.GET_USER_KEY, { did, type, key });
-        return wallet.key;
+        /**
+          * This block gets a wallet by sending wallet data to Auth Service
+          * through WalletEvents.GET_USER_KEY message
+          * 
+            const wallet = await this.request<any>(WalletEvents.GET_USER_KEY, { did, type, key });
+            return wallet.key;
+         */
+
+        /**
+         * This block get user walletToken from Auth Service and then gets the wallet directly 
+         */
+        const user = new Users();
+        const { walletToken } = await user.getUserById(did);
+        
+        const wallet = new WalletManager();
+        return await wallet.getKey(walletToken, type, key);
     }
 
     /**
@@ -68,6 +105,20 @@ export class Wallet extends ServiceRequestsBase {
      * @param value
      */
     public async setUserKey(did: string, type: KeyType, key: string, value: any) {
-        await this.request<any>(WalletEvents.SET_USER_KEY, { did, type, key, value });
+        /**
+         * This block create/update a wallet by sending wallet data to Auth Service
+         * through WalletEvents.SET_USER_KEY message
+         * 
+            await this.request<any>(WalletEvents.SET_USER_KEY, { did, type, key, value });
+         */
+
+        /**
+         * This block get user walletToken from Auth Service and then creates/updates the wallet directly 
+         */
+        const user = new Users();
+        const { walletToken } = await user.getUserById(did);
+        
+        const wallet = new WalletManager();
+        await wallet.setKey(walletToken, type, key, value);
     }
 }
