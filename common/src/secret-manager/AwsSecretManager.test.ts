cd7364cd44f0a08ada81eada1a878367bff39c29
hnamzian <namazian87@gmail.com>
2023-02-07 16:41:35 +0300

    Tested AWS Secrets Manager with all paths

---
+++ common/src/secret-manager/AwsSecretManager.test.ts
@@ -0,0 +1,34 @@
+import { SecretManager } from "./SecretManager";
+import { SecretManagerType } from "./SecretManagerConfig";
+
+process.env.AWS_REGION = 'eu-central-1';
+
+async function test_aws_secretmanager() {
+  const secretManager = SecretManager.New(SecretManagerType.AWS)
+
+  await secretManager.setSecrets("apikey/ipfs", {
+    IPFS_STORAGE_API_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweGM2YzJlMzg2YkU0QzBlRGY5MEMwZjE2MjIxRmYyMTgxMjY1OEQyYTUiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2NjY2MDM3Njc2MTMsIm5hbWUiOiJHdWFyZGlhbiBUZXN0In0.qw0O9fdFSXsrFWm5Wk75OdayKq5tSCg0_iB4c0Ixd7J'
+  })
+
+  const { IPFS_STORAGE_API_KEY } = await secretManager.getSecrets("apikey/ipfs")
+  console.log(IPFS_STORAGE_API_KEY);
+
+
+  await secretManager.setSecrets("secretkey/auth", {
+    ACCESS_TOKEN_SECRET: 'weakpassword'
+  })
+
+  const { ACCESS_TOKEN_SECRET } = await secretManager.getSecrets("secretkey/auth")
+  console.log(ACCESS_TOKEN_SECRET);
+
+
+  await secretManager.setSecrets("keys/operator", {
+    OPERATOR_ID: '0.0.12345678',
+    OPERATOR_KEY: '0x1276237349870385464852193749'
+  })
+
+  const { OPERATOR_ID, OPERATOR_KEY } = await secretManager.getSecrets("keys/operator")
+  console.log(OPERATOR_ID, OPERATOR_KEY);
+}
+
+test_aws_secretmanager()
