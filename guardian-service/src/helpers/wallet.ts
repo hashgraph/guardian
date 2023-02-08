f6800128c53b82238ca36fd7ca66ee469fbae2fe
hnamzian <namazian87@gmail.com>
2023-02-08 12:04:35 +0300

    Removed comments of previous implementations in wallet class

--- guardian-service/src/helpers/wallet.ts
+++ guardian-service/src/helpers/wallet.ts
@@ -1,6 +1,5 @@
 import { Singleton } from '@helpers/decorators/singleton';
 import { ServiceRequestsBase } from '@helpers/service-requests-base';
-// import { WalletEvents, IGetKeyResponse } from '@guardian/interfaces';
 import { Wallet as WalletManager} from '@guardian/common'
 import { Users } from './users';
 
@@ -37,16 +36,6 @@ export class Wallet extends ServiceRequestsBase {
      * @param key
      */
     public async getKey(token: string, type: KeyType, key: string): Promise<string> {
-        /**
-         * This block sends WalletEvents.GET_KEY to Auth Service to get Wallet PrivateKey in response
-         * 
-            const wallet = await this.request<IGetKeyResponse>(WalletEvents.GET_KEY, { token, type, key });
-            return wallet.key;
-         */
-
-        /**
-         * This block gets Wallet PrivateKey from Vault directly
-         */
         const wallet = new WalletManager();
         return await wallet.getKey(token, type, key);
     }
@@ -59,16 +48,6 @@ export class Wallet extends ServiceRequestsBase {
      * @param value
      */
     public async setKey(token: string, type: string, key: string, value: string) {
-        /**
-         * This block create/update a wallet by sending wallet data to Auth Service
-         * through WalletEvents.SET_KEY message
-         * 
-            await this.request<any>(WalletEvents.SET_KEY, { token, type, key, value });
-         */
-
-        /**
-         * This block creates/updates a Wallet by writing to Vault directly
-         */
         const wallet = new WalletManager();
         await wallet.setKey(token, type, key, value);
     }
@@ -79,17 +58,6 @@ export class Wallet extends ServiceRequestsBase {
      * @param key
      */
      public async getUserKey(did: string, type: KeyType, key: string): Promise<any> {
-        /**
-          * This block gets a wallet by sending wallet data to Auth Service
-          * through WalletEvents.GET_USER_KEY message
-          * 
-            const wallet = await this.request<any>(WalletEvents.GET_USER_KEY, { did, type, key });
-            return wallet.key;
-         */
-
-        /**
-         * This block get user walletToken from Auth Service and then gets the wallet directly 
-         */
         const user = new Users();
         const { walletToken } = await user.getUserById(did);
         
@@ -105,16 +73,6 @@ export class Wallet extends ServiceRequestsBase {
      * @param value
      */
     public async setUserKey(did: string, type: KeyType, key: string, value: any) {
-        /**
-         * This block create/update a wallet by sending wallet data to Auth Service
-         * through WalletEvents.SET_USER_KEY message
-         * 
-            await this.request<any>(WalletEvents.SET_USER_KEY, { did, type, key, value });
-         */
-
-        /**
-         * This block get user walletToken from Auth Service and then creates/updates the wallet directly 
-         */
         const user = new Users();
         const { walletToken } = await user.getUserById(did);
         
