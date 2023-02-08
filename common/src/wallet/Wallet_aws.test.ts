81f5984900349c9a2e34441bc34a3e6d2278463a
hnamzian <namazian87@gmail.com>
2023-02-08 07:48:46 +0300

    Wallet tested against AWS secret manager

---
+++ common/src/wallet/Wallet_aws.test.ts
@@ -0,0 +1,21 @@
+import { Wallet } from "./Wallet";
+import { SecretManagerType } from "../secret-manager";
+
+process.env.AWS_REGION = 'eu-central-1'
+
+async function test_wallet_by_guardian() {
+  console.log("Test Wallet by Guardian")
+  
+
+  const wallet = new Wallet(SecretManagerType.AWS)
+  await wallet.setKey("token1", "OPERATOR", "privateKey", "123456")
+  const data = await wallet.getKey("token1", "OPERATOR", "privateKey")
+
+  console.log(data);
+}
+
+async function test_wallet() {
+  await test_wallet_by_guardian()
+} 
+
+test_wallet()
