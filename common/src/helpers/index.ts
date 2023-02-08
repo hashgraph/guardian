d2f56d6fbeea76592e1ec990d4e193cc026653cf
hnamzian <namazian87@gmail.com>
2023-02-08 11:59:18 +0300

    Removed settings-container as secrets are now handled by secret-manager

--- common/src/helpers/index.ts
+++ common/src/helpers/index.ts
@@ -2,7 +2,6 @@ export * from './application-state';
 export * from './logger';
 export * from './db-helper';
 export * from './migration';
-export * from './settings-container';
 export * from './service-requests-base';
 export * from './run-function-async'
 export * from './do-nothing'
