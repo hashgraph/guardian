b3433852410cc5e6f1d2ff49f7702b468d4b0b8f
otherNet <francesco.calderoni@intellecteu.com>
2023-03-09 13:21:49 +0100

    resolve #1923,resolve #1693 Integrate multi env feature Service Env can Override configs folder in dockerfile & managed GUARDIAN_ENV docs Docs Manual execution Apply changes on updated develop DIRECT_MESSAGE_PORT is a specilized variable
    Signed-off-by: otherNet <francesco.calderoni@intellecteu.com>

--- logger-service/src/config.ts
+++ logger-service/src/config.ts
@@ -1,5 +1,6 @@
 import moduleAlias from 'module-alias';
 import dotenv from 'dotenv';
+import fs from 'fs';
 
 moduleAlias.addAliases({
     '@api': __dirname + '/api',
@@ -7,3 +8,24 @@ moduleAlias.addAliases({
 });
 
 dotenv.config();
+
+const envPath = process.env.GUARDIAN_ENV ? `./configs/.env.logger.${process.env.GUARDIAN_ENV}` : './configs/.env.logger';
+
+if (!process.env.OVERRIDE || process.env.OVERRIDE === 'false'){
+    console.log('reading from', envPath, 'not overriding');
+    dotenv.config({ path: envPath});
+}else{
+    try {
+        const envConfig = dotenv.parse(fs.readFileSync(envPath));
+        for (const k of Object.keys(envConfig)) {
+            process.env[k] = envConfig[k]
+        }
+    } catch (err) {
+        if (err.code === 'ENOENT') {
+            console.log('WARN: Specific environment not loaded');
+        } else {
+            throw err;
+        }
+    }
+}
+console.log('Charged Environment',process.env,'\r\n___ . ___');
