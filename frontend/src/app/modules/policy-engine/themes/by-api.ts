0150b055f62e7a3f304e57a822273b727c4fd048
Stepan Kiryakov <stepan.kiryakov@envisionblockchain.com>
2023-04-19 18:19:20 +0400

    update swagger
    Signed-off-by: Stepan Kiryakov <stepan.kiryakov@envisionblockchain.com>

--- frontend/src/app/modules/policy-engine/themes/by-api.ts
+++ frontend/src/app/modules/policy-engine/themes/by-api.ts
@@ -1,38 +1,38 @@
 export const byApiTheme = {
-    id: 2,
-    uuid: '00000000-0000-0000-0000-000000000002',
-    readonly: true,
-    name: "By API Access",
-    rules: [
+    'id': 2,
+    'uuid': '00000000-0000-0000-0000-000000000002',
+    'readonly': true,
+    'name': 'By API Access',
+    'rules': [
         {
-            description: "POST & GET",
-            text: "#031d00",
-            background: "#3dff46",
-            border: "#00a316",
-            shape: "0",
-            borderWidth: "2px",
-            filterType: "api",
-            filterValue: "post"
+            'description': 'POST & GET',
+            'text': '#031d00',
+            'background': '#b8ffc7',
+            'border': '#16bc2d',
+            'shape': '0',
+            'borderWidth': '2px',
+            'filterType': 'api',
+            'filterValue': 'post'
         },
         {
-            description: "Only GET",
-            text: "#1e1f00",
-            background: "#fff67a",
-            border: "#c2a800",
-            shape: "0",
-            borderWidth: "2px",
-            filterType: "api",
-            filterValue: "get"
+            'description': 'Only GET',
+            'text': '#1e1f00',
+            'background': '#fff899',
+            'border': '#d0b60c',
+            'shape': '1',
+            'borderWidth': '2px',
+            'filterType': 'api',
+            'filterValue': 'get'
         },
         {
-            description: "Not Accessible",
-            text: "#6a6a6a",
-            background: "#ffecec",
-            border: "#e26868",
-            shape: "3",
-            borderWidth: "2px",
-            filterType: "api",
-            filterValue: ""
+            'description': 'Not Accessible',
+            'text': '#474747',
+            'background': '#ffefef',
+            'border': '#e97474',
+            'shape': '3',
+            'borderWidth': '2px',
+            'filterType': 'api',
+            'filterValue': ''
         }
     ]
 }
--- frontend/src/app/modules/policy-engine/themes/by-roles.ts
+++ frontend/src/app/modules/policy-engine/themes/by-roles.ts
@@ -1,67 +1,285 @@
 export const byRolesTheme = {
-    id: 3,
-    uuid: '00000000-0000-0000-0000-000000000003',
-    readonly: true,
-    name: 'By Roles',
-    rules: [
+    'id': 3,
+    'uuid': '00000000-0000-0000-0000-000000000003',
+    'readonly': true,
+    'name': 'By Roles',
+    'rules': [
         {
-            description: "Role 1",
-            text: "#000",
-            background: "#b3e5fc",
-            border: "#0288d1",
-            shape: "0",
-            borderWidth: "2px",
-            filterType: "role",
-            filterValue: "0"
+            'description': 'Role 1',
+            'text': '#000',
+            'background': '#d6f2ff',
+            'border': '#0288d1',
+            'shape': '0',
+            'borderWidth': '2px',
+            'filterType': 'role',
+            'filterValue': [
+                '0'
+            ]
         },
         {
-            description: "Role 2",
-            text: "#000",
-            background: "#c3efed",
-            border: "#00796b",
-            shape: "0",
-            borderWidth: "2px",
-            filterType: "role",
-            filterValue: "1"
+            'description': 'Role 2',
+            'text': '#000',
+            'background': '#d0fffd',
+            'border': '#00796b',
+            'shape': '0',
+            'borderWidth': '2px',
+            'filterType': 'role',
+            'filterValue': [
+                '1'
+            ]
         },
         {
-            description: "Role 3",
-            text: "#000",
-            background: "#edd2f1",
-            border: "#7b1fa2",
-            shape: "0",
-            borderWidth: "2px",
-            filterType: "role",
-            filterValue: "2"
+            'description': 'Role 3',
+            'text': '#000',
+            'background': '#fbe2ff',
+            'border': '#7b1fa2',
+            'shape': '0',
+            'borderWidth': '2px',
+            'filterType': 'role',
+            'filterValue': [
+                '2'
+            ]
         },
         {
-            description: "Owner",
-            text: "#000",
-            background: "#c8e6c9",
-            border: "#43a047",
-            shape: "0",
-            borderWidth: "2px",
-            filterType: "role",
-            filterValue: "OWNER"
+            'description': 'Role 4',
+            'text': '#000',
+            'background': '#fbffce',
+            'border': '#afb42b',
+            'shape': '0',
+            'borderWidth': '2px',
+            'filterType': 'role',
+            'filterValue': [
+                '3'
+            ]
         },
         {
-            description: "Any Role",
-            text: "#000",
-            background: "#ffecb3",
-            border: "#c47b00",
-            shape: "1",
-            borderWidth: "2px",
-            filterType: "role",
-            filterValue: "ANY_ROLE"
+            'description': 'Role 5',
+            'text': '#000',
+            'background': '#ffead3',
+            'border': '#ff8622',
+            'shape': '0',
+            'borderWidth': '2px',
+            'filterType': 'role',
+            'filterValue': [
+                '4'
+            ]
         },
         {
-            default: true,
-            description: "Other",
-            text: "#000",
-            background: "#fff",
-            border: "#000",
-            shape: "4",
-            borderWidth: "2px"
+            'description': 'Role 6',
+            'text': '#000',
+            'background': '#ffcadf',
+            'border': '#ff4785',
+            'shape': '0',
+            'borderWidth': '2px',
+            'filterType': 'role',
+            'filterValue': [
+                '5'
+            ]
+        },
+        {
+            'description': 'Role 7',
+            'text': '#000',
+            'background': '#bcffd9',
+            'border': '#1dd267',
+            'shape': '0',
+            'borderWidth': '2px',
+            'filterType': 'role',
+            'filterValue': [
+                '6'
+            ]
+        },
+        {
+            'description': 'Role 8',
+            'text': '#ffffff',
+            'background': '#0288d1',
+            'border': '#1565c0',
+            'shape': '0',
+            'borderWidth': '2px',
+            'filterType': 'role',
+            'filterValue': [
+                '7'
+            ]
+        },
+        {
+            'description': 'Role 9',
+            'text': '#ffffff',
+            'background': '#00acc1',
+            'border': '#0097a7',
+            'shape': '0',
+            'borderWidth': '2px',
+            'filterType': 'role',
+            'filterValue': [
+                '8'
+            ]
+        },
+        {
+            'description': 'Role 10',
+            'text': '#ffffff',
+            'background': '#d020ff',
+            'border': '#b11eef',
+            'shape': '0',
+            'borderWidth': '2px',
+            'filterType': 'role',
+            'filterValue': [
+                '9'
+            ]
+        },
+        {
+            'description': 'Role 11',
+            'text': '#ffffff',
+            'background': '#8f9500',
+            'border': '#9e9d24',
+            'shape': '0',
+            'borderWidth': '2px',
+            'filterType': 'role',
+            'filterValue': [
+                '10'
+            ]
+        },
+        {
+            'description': 'Role 12',
+            'text': '#ffffff',
+            'background': '#b36400',
+            'border': '#814200',
+            'shape': '0',
+            'borderWidth': '2px',
+            'filterType': 'role',
+            'filterValue': [
+                '11'
+            ]
+        },
+        {
+            'description': 'Role 13',
+            'text': '#ffffff',
+            'background': '#b4006f',
+            'border': '#490042',
+            'shape': '0',
+            'borderWidth': '2px',
+            'filterType': 'role',
+            'filterValue': [
+                '12'
+            ]
+        },
+        {
+            'description': 'Role 14',
+            'text': '#ffffff',
+            'background': '#7400ce',
+            'border': '#7b1fa2',
+            'shape': '0',
+            'borderWidth': '2px',
+            'filterType': 'role',
+            'filterValue': [
+                '13'
+            ]
+        },
+        {
+            'description': 'Role 15',
+            'text': '#ffffff',
+            'background': '#008502',
+            'border': '#053700',
+            'shape': '0',
+            'borderWidth': '2px',
+            'filterType': 'role',
+            'filterValue': [
+                '14'
+            ]
+        },
+        {
+            'description': 'Role 16',
+            'text': '#ffffff',
+            'background': '#8ed600',
+            'border': '#768100',
+            'shape': '0',
+            'borderWidth': '2px',
+            'filterType': 'role',
+            'filterValue': [
+                '15'
+            ]
+        },
+        {
+            'description': 'Role 17',
+            'text': '#ffffff',
+            'background': '#1c00ff',
+            'border': '#7b1fa2',
+            'shape': '0',
+            'borderWidth': '2px',
+            'filterType': 'role',
+            'filterValue': [
+                '16'
+            ]
+        },
+        {
+            'description': 'Role 18',
+            'text': '#ff0000',
+            'background': '#ffffff',
+            'border': '#ff0000',
+            'shape': '0',
+            'borderWidth': '2px',
+            'filterType': 'role',
+            'filterValue': [
+                '17'
+            ]
+        },
+        {
+            'description': 'Role 19',
+            'text': '#1e00da',
+            'background': '#ffffff',
+            'border': '#001dd0',
+            'shape': '0',
+            'borderWidth': '2px',
+            'filterType': 'role',
+            'filterValue': [
+                '18'
+            ]
+        },
+        {
+            'description': 'Role 20',
+            'text': '#008d13',
+            'background': '#ffffff',
+            'border': '#008b00',
+            'shape': '0',
+            'borderWidth': '2px',
+            'filterType': 'role',
+            'filterValue': [
+                '19'
+            ]
+        },
+        {
+            'description': 'Owner',
+            'text': '#000000',
+            'background': '#d6f9af',
+            'border': '#388e3c',
+            'shape': '1',
+            'borderWidth': '2px',
+            'filterType': 'role',
+            'filterValue': [
+                'OWNER'
+            ]
+        },
+        {
+            'description': 'Any Role',
+            'text': '#000',
+            'background': '#ffecb3',
+            'border': '#c47b00',
+            'shape': '2',
+            'borderWidth': '2px',
+            'filterType': 'role',
+            'filterValue': [
+                'ANY_ROLE'
+            ]
+        },
+        {
+            'description': '',
+            'text': '#000',
+            'background': '#fff',
+            'border': '#7e7e7e',
+            'shape': '3',
+            'borderWidth': '2px',
+            'filterType': 'role',
+            'filterValue': [
+                'NO_ROLE'
+            ],
+            'default': false
         }
     ]
 }
