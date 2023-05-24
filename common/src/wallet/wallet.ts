05097ac69558f759e358dd3cc4c849aee2421a73
hnamzian <namazian87@gmail.com>
2023-05-23 23:18:34 -0400

    Convert wallet secret key to base58 to prvent gcp key name policy violation

--- common/src/wallet/wallet.ts
+++ common/src/wallet/wallet.ts
@@ -2,6 +2,7 @@ import { SecretManager, SecretManagerBase, SecretManagerType } from '../secret-m
 import crypto from 'crypto';
 import * as bs58 from 'bs58';
 import { AzureSecretManager } from '../secret-manager/azure/azure-secret-manager';
+import { GcpSecretManager } from '../secret-manager/gcp/gcp-secret-manager';
 
 /**
  * Class to manage wallet by Secret Manager Resources
@@ -61,6 +62,11 @@ export class Wallet {
     }
 
     // convert hashedKey from hex to Base58 to shoeten key length, Azure does not accept keys longet than 128 chars
+    if (!(this.secretManager instanceof GcpSecretManager)) {
+      return hashedKey;
+    }
+
+    // convert hashedKey from hex to Base58 to shoeten key length, GCP does not accept keys longet than 255 chars
     const buffer = Buffer.from(hashedKey, 'hex');
     const hashedKeyBase58 = bs58.encode(buffer);
     return hashedKeyBase58;
