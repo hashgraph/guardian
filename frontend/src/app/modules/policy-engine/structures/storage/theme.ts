ab4acc36b42ea5dcb30e93c6234c575139b6de79
Artem Buslaev <artem.buslaev@envisionblockchain.com>
2023-06-14 18:09:36 +0400

    added policy syntax groups constant
    Signed-off-by: Artem Buslaev <artem.buslaev@envisionblockchain.com>

--- frontend/src/app/modules/policy-engine/structures/storage/theme.ts
+++ frontend/src/app/modules/policy-engine/structures/storage/theme.ts
@@ -1,6 +1,7 @@
 import { GenerateUUIDv4 } from '@guardian/interfaces';
 import { PolicyBlockModel } from '..';
 import { ThemeRule } from './theme-rule';
+import { DEFAULT_SYNTAX_GROUPS } from '../../themes/default-syntax-groups';
 
 export interface SyntaxGroup {
     id: string;
@@ -30,68 +31,7 @@ export class Theme {
         this._defaultRole.default = true;
         this._defaultRole.description = 'Default';
         this._colorMap = new Map();
-        this._syntaxGroups = [
-            {
-                id: 'policy-id',
-                name: 'Identifiers',
-                color: '#3F51B5'
-            },
-            {
-                id: 'policy-name',
-                name: 'Names',
-                color: '#9C27B0'
-            },
-            {
-                id: 'policy-type',
-                name: 'Types',
-                color: '#795548'
-            },
-            {
-                id: 'policy-version',
-                name: 'Versions',
-                color: 'darkgoldenrod'
-            },
-            {
-                id: 'policy-user',
-                name: 'Users',
-                color: 'darkolivegreen'
-            },
-            {
-                id: 'policy-tag',
-                name: 'Tags',
-                color: 'darkslategray'
-            },
-            {
-                id: 'policy-complex',
-                name: 'Complex Objects',
-                color: 'orchid'
-            },
-            {
-                id: 'policy-simple',
-                name: 'Simple Objects',
-                color: 'midnightblue'
-            },
-            {
-                id: 'policy-date',
-                name: 'Dates',
-                color: 'salmon'
-            },
-            {
-                id: 'policy-array',
-                name: 'Arrays',
-                color: 'darkslateblue'
-            },
-            {
-                id: 'policy-flag',
-                name: 'Flags',
-                color: 'green'
-            },
-            {
-                id: 'policy-error',
-                name: 'Errors',
-                color: 'crimson'
-            },
-        ];
+        this._syntaxGroups = DEFAULT_SYNTAX_GROUPS;
     }
 
     public get name(): string {
--- frontend/src/app/modules/policy-engine/themes/by-api.ts
+++ frontend/src/app/modules/policy-engine/themes/by-api.ts
@@ -1,3 +1,5 @@
+import { DEFAULT_SYNTAX_GROUPS } from "./default-syntax-groups";
+
 export const byApiTheme = {
     'id': 2,
     'uuid': '00000000-0000-0000-0000-000000000002',
@@ -35,66 +37,5 @@ export const byApiTheme = {
             'filterValue': ''
         }
     ],
-    syntaxGroups: [
-        {
-            id: 'policy-id',
-            name: 'Identifiers',
-            color: '#3F51B5'
-        },
-        {
-            id: 'policy-name',
-            name: 'Names',
-            color: '#9C27B0'
-        },
-        {
-            id: 'policy-type',
-            name: 'Types',
-            color: '#795548'
-        },
-        {
-            id: 'policy-version',
-            name: 'Versions',
-            color: 'darkgoldenrod'
-        },
-        {
-            id: 'policy-user',
-            name: 'Users',
-            color: 'darkolivegreen'
-        },
-        {
-            id: 'policy-tag',
-            name: 'Tags',
-            color: 'darkslategray'
-        },
-        {
-            id: 'policy-complex',
-            name: 'Complex Objects',
-            color: 'orchid'
-        },
-        {
-            id: 'policy-simple',
-            name: 'Simple Objects',
-            color: 'midnightblue'
-        },
-        {
-            id: 'policy-date',
-            name: 'Dates',
-            color: 'salmon'
-        },
-        {
-            id: 'policy-array',
-            name: 'Arrays',
-            color: 'darkslateblue'
-        },
-        {
-            id: 'policy-flag',
-            name: 'Flags',
-            color: 'green'
-        },
-        {
-            id: 'policy-error',
-            name: 'Errors',
-            color: 'crimson'
-        },
-    ]
+    'syntaxGroups': DEFAULT_SYNTAX_GROUPS,
 }
