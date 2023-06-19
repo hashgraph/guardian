20e8579af8f5d90bc38fb2f096db9718c5186396
hnamzian <namazian87@gmail.com>
2023-06-19 19:54:19 -0400

    Get my user profile by the use of meeco-api class

--- auth-service/meeco/meeco-provider.ts
+++ auth-service/meeco/meeco-provider.ts
@@ -1,5 +1,6 @@
 import { Cryppo } from "./cryppo";
 import { IMeecoConfig, MeecoApi } from "./meeco-api";
+import { IMe } from "./models/me";
 
 /**
  * MeecoProvider is a wrapper around the Meeco API and Cryppo.
@@ -12,5 +13,14 @@ export class MeecoProvider {
   constructor(config: IMeecoConfig, passphraseBase32: string) {
     this.meecoApi = new MeecoApi(config);
     this.cryppo = new Cryppo(passphraseBase32);
+  }
+
+  /**
+   * getMe returns the Meeco user profile.
+   * @returns {IMe} Meeco user profile
+   */
+  async getMe(): Promise<IMe> {
+    const me = await this.meecoApi.getMe();
+    return me;
   }
 }
