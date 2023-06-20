6088156711e2834fa9356510516b02bb877106b5
varsha0592 <varsha.vasudevan@intellecteu.com>
2023-06-20 11:32:39 +0200

    Tokens
    Signed-off-by: varsha0592 <varsha.vasudevan@intellecteu.com>

--- e2e-tests/cypress.config.js
+++ e2e-tests/cypress.config.js
@@ -7,14 +7,13 @@ module.exports = defineConfig({
   watchForFileChanges: false,
   defaultCommandTimeout: 10000,
   e2e: {
-    
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
       require('cypress-grep/src/plugin')(config);
       return config;
     },
-    
+    experimentalRunAllSpecs : true,
   },
 })
 
