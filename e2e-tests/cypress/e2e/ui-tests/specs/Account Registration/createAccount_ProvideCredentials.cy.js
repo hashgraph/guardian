c6f14a38a35fd459d91bd2e8c9713247ad15220a
varsha0592 <varsha.vasudevan@intellecteu.com>
2023-06-22 16:58:25 +0200

    update code for Account Registration
    Signed-off-by: lozytskyiintellecteu <oleksandr.lozytskyi@intellecteu.com>

--- e2e-tests/cypress/e2e/ui-tests/specs/Account Registration/createAccount_ProvideCredentials.cy.js
+++ e2e-tests/cypress/e2e/ui-tests/specs/Account Registration/createAccount_ProvideCredentials.cy.js
@@ -1,18 +1,17 @@
 import { AuthenticationPage } from "../../pages/authentication";
 import { HomePage } from "../../pages/homepage";
 import { ConfigPage } from "../../pages/configpage";
-
+import { slowCypressDown } from 'cypress-slow-down'
 
 const home = new AuthenticationPage();
 const homepage = new HomePage();
 const configpage = new ConfigPage();
 
 describe("Create User Accounts",  { tags: '@ui' }, () => {
-   
+    slowCypressDown(320) ;
     const admin =  "ADMIN"+ Math.floor(Math.random() * 999);
-    const auditor =  "AUDITOR"+ Math.floor(Math.random() * 999);
-    const HederaIDSD = "0.0.3979374";
-    const HederaKeySD = "302e020100300506032b657004220420d13eb8a1b4135a971863f6dc6eccaa53d47e829740776e9d354aa7252dec6b7d";
+    const HederaIDSD = "0.0.3978325";
+    const HederaKeySD = "302e020100300506032b65700422042074cf1316884a78ccb24b9a3313cf800f297b1c06a2b089829602b3a23edbf55c";
     const HederaIDUser = "0.0.3979436";
     const HederaKeyUser = "302e020100300506032b657004220420f88a133b2ab50f885f0913a0c37daf05af216f557a32fdb8354370e81967733d";
   
@@ -21,21 +20,10 @@ describe("Create User Accounts",  { tags: '@ui' }, () => {
         home.visit();
     })
     
-    it("Create Standard Registry by generating Hedera ID and KEY", () => {
-  
-       
-       homepage.createStandartRegistryAccount(admin);
-        cy.wait(3000);
-        var option = "GENERATE";
-        configpage.finishsetupSD(option);
-        configpage.verifyHeaderLabelsOnLoginPageForAdmin();
-        home.logOut(admin);
 
-    });
 
     //ID and KEY has to be set unique for each Test
     it("Create Standard Registry by providing Hedera ID and KEY", () => {
-        var admin =  "ADMIN"+ Math.floor(Math.random() * 999);
        homepage.createStandartRegistryAccount(admin);
         cy.wait(3000);
         cy.wait(3000);
@@ -46,16 +34,7 @@ describe("Create User Accounts",  { tags: '@ui' }, () => {
 
     });
 
-    it("Create User by generating Hedera ID and KEY", () => {
-        var username =  "USER"+ Math.floor(Math.random() * 999);
-       homepage.createUserAccount(username);
-        cy.wait(3000);
-        var option = "GENERATE";
-        configpage.finishsetupUser(admin,username,option);
-            configpage.verifyHeaderLabelsOnLoginPageForUser();
-        home.logOut(username);
-
-    });
+  
 
         //ID and KEY has to be set unique for each Test
     it("Create User by providing Hedera ID and KEY", () => {
@@ -69,15 +48,6 @@ describe("Create User Accounts",  { tags: '@ui' }, () => {
 
     });
 
-    it("Create Auditor", () => {
-        homepage.createAuditor(auditor);
-        cy.wait(3000);
-        home.logOut(auditor);
-
-    });
-
-
-
-    
+ 
    
 });
