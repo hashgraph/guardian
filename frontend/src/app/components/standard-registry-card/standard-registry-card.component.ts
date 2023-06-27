7194eb3390eb6a027c8136b4468fcb7103c9b836
Stepan Kiryakov <stepan.kiryakov@envisionblockchain.com>
2023-06-27 17:14:29 +0400

    fix style
    Signed-off-by: Stepan Kiryakov <stepan.kiryakov@envisionblockchain.com>

--- frontend/src/app/components/standard-registry-card/standard-registry-card.component.ts
+++ frontend/src/app/components/standard-registry-card/standard-registry-card.component.ts
@@ -16,14 +16,14 @@ export class StandardRegistryCardComponent {
     @Output() registrySelected: EventEmitter<string> = new EventEmitter<string>();
 
     constructor(private matIconRegistry: MatIconRegistry, private domSanitizer: DomSanitizer) {
-      this.matIconRegistry.addSvgIconLiteral(
-        'chevron_down',
-        this.domSanitizer.bypassSecurityTrustHtml(`
-        <svg xmlns="http://www.w3.org/2000/svg" width="12" height="8" viewBox="0 0 12 8" fill="none">
-            <path d="M6 7.4L0 1.4L1.4 0L6 4.6L10.6 0L12 1.4L6 7.4Z" fill="#222222"/>
-        </svg>
-    `)
-    );
+        this.matIconRegistry.addSvgIconLiteral(
+            'chevron_down',
+            this.domSanitizer.bypassSecurityTrustHtml(`
+                <svg xmlns="http://www.w3.org/2000/svg" width="12" height="8" viewBox="0 0 12 8" fill="none">
+                    <path d="M6 7.4L0 1.4L1.4 0L6 4.6L10.6 0L12 1.4L6 7.4Z" fill="#222222"/>
+                </svg>
+            `)
+        );
     }
 
     onCardClick(): void {
@@ -39,6 +39,6 @@ export class StandardRegistryCardComponent {
     }
 
     get isPoliciesMenuOpened(): boolean {
-        return this.policiesMenuTrigger?.menuOpen;
+        return this.policiesMenuTrigger?.menuOpen || false;
     }
 }
