7a713aac5b0ed7b8ce26e93496eb1f7801e66897
simvalery <valeriy.simonov@envisionblockchain.com>
2023-06-30 17:41:54 +0400

    use static swagger descriptions if need
    Signed-off-by: simvalery <valeriy.simonov@envisionblockchain.com>

--- api-gateway/src/app.ts
+++ api-gateway/src/app.ts
@@ -14,6 +14,7 @@ import { HttpStatus, ValidationPipe } from '@nestjs/common';
 import { json } from 'express';
 import { SwaggerModule } from '@nestjs/swagger';
 import { SwaggerConfig } from '@helpers/swagger-config';
+import { SwaggerModels, SwaggerPaths } from './old-descriptions';
 
 const PORT = process.env.PORT || 3002;
 
@@ -61,6 +62,8 @@ Promise.all([
         new TaskManager().setDependecies(wsService, cn);
 
         const document = SwaggerModule.createDocument(app, SwaggerConfig);
+        Object.assign(document.paths, SwaggerPaths)
+        Object.assign(document.components.schemas, SwaggerModels.schemas);
         SwaggerModule.setup('api-docs', app, document);
 
         const maxPayload = parseInt(process.env.MQ_MAX_PAYLOAD, 10);
