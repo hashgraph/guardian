75fcb4faca341837c78e1e8cf4f434cb89e3e4f7
lozytskyiintellecteu <oleksandr.lozytskyi@intellecteu.com>
2023-07-04 20:30:47 +0800

    moved all in one block of code
    Signed-off-by: lozytskyiintellecteu <oleksandr.lozytskyi@intellecteu.com>

--- e2e-tests/cypress.config.js
+++ e2e-tests/cypress.config.js
@@ -1,35 +1,21 @@
 const { defineConfig } = require('cypress')
 const { verifyDownloadTasks } = require('cy-verify-downloads');
 
-
 module.exports = defineConfig({
   video: false,
   watchForFileChanges: false,
   defaultCommandTimeout: 10000,
   e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
       require('cypress-grep/src/plugin')(config);
+      on('task', verifyDownloadTasks);
       return config;
     },
-    experimentalRunAllSpecs : true,
-  },
-})
-
-module.exports = defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      on('task', verifyDownloadTasks);
-    },
-  },
-});
-
-module.exports = defineConfig({
-  reporter: 'cypress-mochawesome-reporter',
-  e2e: {
+    experimentalRunAllSpecs: true,
+    reporter: 'cypress-mochawesome-reporter',
     setupNodeEvents(on, config) {
       require('cypress-mochawesome-reporter/plugin')(on);
+      return config;
     },
   },
 });
