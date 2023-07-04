41b1d3162d40fd2befeb02c59d8cbfc1029f3e71
lozytskyiintellecteu <oleksandr.lozytskyi@intellecteu.com>
2023-07-04 19:45:34 +0800

    added mochawesome report for cypress ui tests
    Signed-off-by: lozytskyiintellecteu <oleksandr.lozytskyi@intellecteu.com>

--- e2e-tests/cypress.config.js
+++ e2e-tests/cypress.config.js
@@ -24,3 +24,12 @@ module.exports = defineConfig({
     },
   },
 });
+
+module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  e2e: {
+    setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
+    },
+  },
+});
