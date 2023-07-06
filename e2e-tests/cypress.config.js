32deb2cbbdb38980d8b3b75571c2331c71b178de
lozytskyiintellecteu <oleksandr.lozytskyi@intellecteu.com>
2023-07-06 20:45:35 +0800

    moved all in one block of code
    Signed-off-by: lozytskyiintellecteu <oleksandr.lozytskyi@intellecteu.com>

--- e2e-tests/cypress.config.js
+++ e2e-tests/cypress.config.js
@@ -1,48 +1,30 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require('cypress');
 const { verifyDownloadTasks } = require('cy-verify-downloads');
-
 
 module.exports = defineConfig({
   video: false,
   watchForFileChanges: false,
   defaultCommandTimeout: 10000,
   e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
-   setupNodeEvents(on, config) {
-     require('cypress-grep/src/plugin')(config);
-     return config;
-   },
-  },
-})
-
-module.exports = defineConfig({
-  e2e: {
     setupNodeEvents(on, config) {
+      require('cypress-grep/src/plugin')(config);
       on('task', verifyDownloadTasks);
-    },
-  },
-});
 
-module.exports = defineConfig({
-  e2e: {
+      on('task', {
+        checkFile(partialName) {
+          const fs = require('fs');
+          const path = require('path');
+
+          const files = fs.readdirSync(config.env.downloadFolder);
+          const matchingFiles = files.filter(file => file.includes(partialName));
+          return matchingFiles.length > 0;
+        },
+      });
+
+      return config;
+    },
     env: {
-      "downloadFolder": "../e2e-tests/cypress/downloads/"
-    },
-   setupNodeEvents(on, config) {
-    on('task', {
-      checkFile(partialName) {
-        const fs = require('fs');
-        const path = require('path');
-
-        const files = fs.readdirSync(config.env.downloadFolder);
-        const matchingFiles = files.filter(file => file.includes(partialName));
-        return matchingFiles.length > 0;
-      },
-    })
-   },
-  },
-})
-
-
-
+      downloadFolder: '../e2e-tests/cypress/downloads/'
+    }
+  }
+});
