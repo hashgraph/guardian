b29d5a5e9d65f4fa9c8c6f04991ffe845358c054
lozytskyiintellecteu <oleksandr.lozytskyi@intellecteu.com>
2023-07-07 19:46:17 +0800

    merged cypress.config.js file
    Signed-off-by: lozytskyiintellecteu <oleksandr.lozytskyi@intellecteu.com>

--- e2e-tests/cypress.config.js
+++ e2e-tests/cypress.config.js
@@ -1,4 +1,4 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require('cypress');
 const { verifyDownloadTasks } = require('cy-verify-downloads');
 
 module.exports = defineConfig({
@@ -6,16 +6,25 @@ module.exports = defineConfig({
   watchForFileChanges: false,
   defaultCommandTimeout: 10000,
   e2e: {
-    setupNodeEvents(on, config) {
-      require('cypress-grep/src/plugin')(config);
-      on('task', verifyDownloadTasks);
-      return config;
-    },
     experimentalRunAllSpecs: true,
     reporter: 'cypress-mochawesome-reporter',
     setupNodeEvents(on, config) {
+      require('cypress-grep/src/plugin')(config);
       require('cypress-mochawesome-reporter/plugin')(on);
+      on('task', verifyDownloadTasks);
+      on('task', {
+        checkFile(partialName) {
+          const fs = require('fs');
+          const path = require('path');
+          const files = fs.readdirSync(config.env.downloadFolder);
+          const matchingFiles = files.filter(file => file.includes(partialName));
+          return matchingFiles.length > 0;
+        },
+      });
       return config;
     },
-  },
+    env: {
+      downloadFolder: '../e2e-tests/cypress/downloads/'
+    }
+  }
 });
