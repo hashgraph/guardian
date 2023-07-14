841a1452dd7980ffc208205f624cc8b1506d5360
hnamzian <namazian87@gmail.com>
2023-07-14 16:17:02 -0400

    Fixed bug to return JSON parsed object at GcpSecretManager getSecret

---
+++ common/src/secret-manager/gcp/gcp-secret-manager-configs.ts
@@ -0,0 +1,28 @@
+import { SecretManagerConfigsBase } from '../secret-manager-config-base';
+
+/**
+ * Interface for GCP Secret Manager configs
+ */
+export interface IGcpSecretManagerConfigs {
+  /**
+   * GCP region
+   */
+  projectId: string,
+}
+
+/**
+ * Class to get GCP Secret Manager configs
+ */
+export class GcpSecretManagerConfigs implements SecretManagerConfigsBase {
+  /**
+   * Get GCP Secret Manager configs from environment variables
+   * @returns GCP Secret Manager configs
+   * @public
+   * @static
+   */
+  static getConfigs(): IGcpSecretManagerConfigs {
+    return {
+      projectId: process.env.GCP_PROJECT_ID,
+    } as IGcpSecretManagerConfigs;
+  }
+}
