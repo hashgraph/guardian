5b881ff65c5a1edd610a0e904213ac148941a27d
Iryna Telesheva <iryna.telesheva@intellecteu.com>
2023-07-20 17:21:23 +0200

    Update UI, improve meeco-vc-submit-dialog view, styles
    Signed-off-by: Iryna Telesheva <iryna.telesheva@intellecteu.com>

--- frontend/src/app/components/meeco-vc-submit-dialog/meeco-vc-submit-dialog.component.ts
+++ frontend/src/app/components/meeco-vc-submit-dialog/meeco-vc-submit-dialog.component.ts
@@ -1,11 +1,17 @@
 import { Component, Inject } from '@angular/core';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { UserRole } from '@guardian/interfaces';
 import { WebSocketService } from 'src/app/services/web-socket.service';
 
 interface VCSubmitDialogData {
-  document: any;
-  presentationRequestId: string;
-  submissionId: string;
+    document: any;
+    presentationRequestId: string;
+    submissionId: string;
+}
+
+interface RolesData {
+    displayName: string;
+    value: UserRole;
 }
 
 @Component({
@@ -17,21 +23,55 @@ export class MeecoVCSubmitDialogComponent {
     vcSubject: any;
     presentationRequestId: string;
     submissionId: string;
+    roles: RolesData[] = [
+        {
+            displayName: 'Standard Registry',
+            value: UserRole.STANDARD_REGISTRY,
+        },
+        {
+            displayName: 'User',
+            value: UserRole.USER,
+        },
+        {
+            displayName: 'Auditor',
+            value: UserRole.AUDITOR,
+        },
+    ];
+    selectedRole: UserRole;
 
     constructor(
         @Inject(MAT_DIALOG_DATA) private data: VCSubmitDialogData,
+        private dialogRef: MatDialogRef<MeecoVCSubmitDialogComponent>,
         private wsService: WebSocketService
     ) {
-        this.vcSubject = JSON.stringify(this.data.document, null, 4);
+        this.vcSubject = Object.entries(this.data.document);
         this.presentationRequestId = this.data.presentationRequestId;
         this.submissionId = this.data.submissionId;
+
+        this.handleMeecoVerificationFail();
+    }
+
+    private handleMeecoVerificationFail(): void {
+        this.wsService.meecoVerifyVPFailedSubscribe(() => this.closeDialog());
+    }
+
+    private closeDialog(): void {
+        this.dialogRef.close(null);
     }
 
     onReject(): void {
-      this.wsService.rejectVCSubject(this.presentationRequestId, this.submissionId);
+        this.wsService.rejectVCSubject(
+            this.presentationRequestId,
+            this.submissionId
+        );
+        this.closeDialog();
     }
 
     onApprove(): void {
-      this.wsService.approveVCSubject(this.presentationRequestId, this.submissionId);
+        this.wsService.approveVCSubject(
+            this.presentationRequestId,
+            this.submissionId,
+            this.selectedRole
+        );
     }
 }
--- frontend/src/app/components/qr-code-dialog/qr-code-dialog.component.ts
+++ frontend/src/app/components/qr-code-dialog/qr-code-dialog.component.ts
@@ -1,5 +1,6 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnDestroy } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { ToastrService } from 'ngx-toastr';
 import { WebSocketService } from 'src/app/services/web-socket.service';
 
 @Component({
@@ -7,21 +8,39 @@ import { WebSocketService } from 'src/app/services/web-socket.service';
     templateUrl: './qr-code-dialog.component.html',
     styleUrls: ['./qr-code-dialog.component.scss'],
 })
-export class QrCodeDialogComponent {
+export class QrCodeDialogComponent implements OnDestroy {
     qrCodeData: string;
     isMobile: boolean = window.innerWidth <= 810;
 
     constructor(
         @Inject(MAT_DIALOG_DATA) private data: any,
         private dialogRef: MatDialogRef<QrCodeDialogComponent>,
-        private wsService: WebSocketService,
+        private toastr: ToastrService,
+        private wsService: WebSocketService
     ) {
         this.qrCodeData = this.data.qrCodeData;
         this.handleMeecoVerificationFail();
     }
 
+    ngOnDestroy(): void {
+        this.wsService.closeVerifyVPSubscription();
+        this.wsService.closeVerifyVPFailedSubscription();
+    }
+
     handleMeecoVerificationFail(): void {
-        this.wsService.meecoVerifyVPFailedSubscribe(() => this.closeDialog());
+        this.wsService.meecoVerifyVPFailedSubscribe((event) => {
+            this.closeDialog();
+            this.toastr.error(
+                `${event.error}.`,
+                'Submission for VP presentation request failed.',
+                {
+                    timeOut: 10000,
+                    closeButton: true,
+                    positionClass: 'toast-bottom-right',
+                    enableHtml: true,
+                }
+            );
+        });
     }
 
     closeDialog(): void {
