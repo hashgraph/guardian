f0f31315987170587ae2e78bdfceaef502764711
Stepan Kiryakov <stepan.kiryakov@envisionblockchain.com>
2023-10-04 23:25:09 +0400

    fix lint
    Signed-off-by: Stepan Kiryakov <stepan.kiryakov@envisionblockchain.com>

--- guardian-service/src/analytics/compare/comparators/hash-comparator.ts
+++ guardian-service/src/analytics/compare/comparators/hash-comparator.ts
@@ -1,4 +1,4 @@
-import { DatabaseServer, Logger, Policy } from '@guardian/common';
+import { Logger, Policy } from '@guardian/common';
 import { PolicyModel } from '../models/policy.model';
 import { SchemaModel } from '../models/schema.model';
 import { TokenModel } from '../models/token.model';
