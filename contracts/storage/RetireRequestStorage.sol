ac592429b4aa26cccd958f55f338701214dba49e
Artem Buslaev <artem.buslaev@envisionblockchain.com>
2023-10-17 11:39:45 +0400

    retire contracts
    Signed-off-by: Artem Buslaev <artem.buslaev@envisionblockchain.com>

---
+++ contracts/storage/RetireRequestStorage.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.11;
+pragma experimental ABIEncoderV2;
+
+abstract contract RetireRequestStorage {
+    struct RetireTokenRequest {
+        address token;
+        int64 count;
+        int64[] serials;
+    }
+
+    function getRequest(address usr, address[] calldata tokens)
+        public
+        view
+        virtual
+        returns (RetireTokenRequest[] memory);
+
+    function setRequest(address usr, RetireTokenRequest[] calldata tokens)
+        public
+        virtual;
+
+    function unsetRequest(address usr, address[] memory tokens)
+        public
+        virtual;
+
+    function getRequests(uint8 tc) external view virtual returns (bytes memory);
+}
