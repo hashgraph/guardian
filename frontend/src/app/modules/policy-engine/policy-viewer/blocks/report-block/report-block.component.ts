c7aa098c6360a3971d061b5fdd2f140c15026c8a
simvalery <valeriy.simonov@envisionblockchain.com>
2023-11-22 20:32:33 +0400

    VP section header for report-block
    Signed-off-by: simvalery <valeriy.simonov@envisionblockchain.com>

--- frontend/src/app/modules/policy-engine/policy-viewer/blocks/report-block/report-block.component.ts
+++ frontend/src/app/modules/policy-engine/policy-viewer/blocks/report-block/report-block.component.ts
@@ -3,16 +3,7 @@ import { FormBuilder, Validators } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import { MatIconRegistry } from '@angular/material/icon';
 import { DomSanitizer } from '@angular/platform-browser';
-import {
-    IImpactReport,
-    IconType,
-    IPolicyReport,
-    IReport,
-    IReportItem,
-    ITokenReport,
-    IVCReport,
-    IVPReport
-} from '@guardian/interfaces';
+import { IconType, IImpactReport, IPolicyReport, IReport, IReportItem, ITokenReport, IVCReport, IVPReport } from '@guardian/interfaces';
 import { VCViewerDialog } from 'src/app/modules/schema-engine/vc-dialog/vc-dialog.component';
 import { IPFSService } from 'src/app/services/ipfs.service';
 import { PolicyEngineService } from 'src/app/services/policy-engine.service';
@@ -52,6 +43,7 @@ export class ReportBlockComponent implements OnInit {
     searchForm = this.fb.group({
         value: ['', Validators.required],
     });
+    uiMetaData: { [key: string]: string };
 
     constructor(
         private policyEngineService: PolicyEngineService,
@@ -69,6 +61,7 @@ export class ReportBlockComponent implements OnInit {
     }
 
     ngOnInit(): void {
+        console.log(this);
         if (!this.static) {
             this.socket = this.wsService.blockSubscribe(this.onUpdate.bind(this));
         }
@@ -123,7 +116,7 @@ export class ReportBlockComponent implements OnInit {
     }
 
     loadTrustChainData(data: any) {
-        const uiMetaData = data.uiMetaData || {};
+        this.uiMetaData = data.uiMetaData || {};
         const report = data.data as IReport;
         this.hash = data.hash;
         this.searchForm.patchValue({
@@ -203,7 +196,7 @@ export class ReportBlockComponent implements OnInit {
     }
 
     openVCDocument(
-        item: any, 
+        item: any,
         document?: any
     ) {
         const dialogRef = this.dialog.open(VCViewerDialog, {
