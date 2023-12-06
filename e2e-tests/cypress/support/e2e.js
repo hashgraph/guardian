069ce12e8f305d1f1a7e32ab7332d9885027a667
Celiant <nikita.nikishin@envisionblockchain.com>
2023-12-06 17:52:46 +0400

    fixing for log in with refresh tokens

--- e2e-tests/cypress/support/e2e.js
+++ e2e-tests/cypress/support/e2e.js
@@ -34,40 +34,57 @@ const authorization = Cypress.env("authorization");
 
 //If StandardRegistry doesn't have hedera credentials, creating them
 before(() => {
-    let username = "StandardRegistry";
     cy.request({
-        method: "GET",
-        url: API.ApiServer + "profiles/" + username,
-        headers: {
-            authorization: authorization,
-        },
-    }).then((response) => {
-        if (response.body.confirmed === false) {
+        method: "POST",
+        url: API.ApiServer + "accounts/login",
+        body: {
+            username: "StandardRegistry",
+            password: "test"
+        }
+    }).then((responseWithRT) => {
+        cy.request({
+            method: "POST",
+            url: API.ApiServer + "accounts/access-token",
+            body: {
+                refreshToken: responseWithRT.body.refreshToken
+            }
+        }).then((responseWithAT) => {
+            let username = "StandardRegistry";
             cy.request({
-                method: "PUT",
+                method: "GET",
                 url: API.ApiServer + "profiles/" + username,
                 headers: {
-                    authorization: authorization,
+                    authorization: "Bearer " + responseWithAT.body.accessToken,
                 },
-                body: {
-                    hederaAccountId: "0.0.469260",
-                    hederaAccountKey:
-                        "3030020100300706052b8104000a04220420fba8a85fb4ed475ab068397da424eb9e2875603c8563d631635733768849a410",
-                    vcDocument: {
-                        geography: "testGeography",
-                        law: "testLaw",
-                        tags: "testTags",
-                        type: "StandardRegistry",
-                        "@context": [],
-                    },
-                },
-                timeout: 200000,
-            }).then(() => {
-                cy.log("hedera credentials was created");
+            }).then((response) => {
+                if (response.body.confirmed === false) {
+                    cy.request({
+                        method: "PUT",
+                        url: API.ApiServer + "profiles/" + username,
+                        headers: {
+                            authorization: authorization,
+                        },
+                        body: {
+                            hederaAccountId: "0.0.469260",
+                            hederaAccountKey:
+                                "3030020100300706052b8104000a04220420fba8a85fb4ed475ab068397da424eb9e2875603c8563d631635733768849a410",
+                            vcDocument: {
+                                geography: "testGeography",
+                                law: "testLaw",
+                                tags: "testTags",
+                                type: "StandardRegistry",
+                                "@context": [],
+                            },
+                        },
+                        timeout: 200000,
+                    }).then(() => {
+                        cy.log("hedera credentials was created");
+                    });
+                } else {
+                    cy.log("User has hedera credentials");
+                }
             });
-        } else {
-            cy.log("User has hedera credentials");
-        }
+        })
     });
 });
 
