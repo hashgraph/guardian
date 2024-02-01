5bc4d82746412110ddf2049a08db50f135e646af
simvalery <valeriy.simonov@envisionblockchain.com>
2024-02-01 13:34:48 +0400

    add onlyForDemo directive
    Signed-off-by: simvalery <valeriy.simonov@envisionblockchain.com>

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -113,6 +113,7 @@ import { ProjectComparisonModule } from './modules/project-comparison/project-co
 import { AngularSvgIconModule } from 'angular-svg-icon';
 // Prototypes
 import '../prototypes/date-prototype';
+import { OnlyForDemoDirective } from './components/onlyfordemo.directive';
 
 @NgModule({
     declarations: [
@@ -152,6 +153,7 @@ import '../prototypes/date-prototype';
         TermsConditionsComponent,
         AccountTypeSelectorDialogComponent,
         ForgotPasswordDialogComponent,
+        OnlyForDemoDirective
     ],
     imports: [
         BrowserModule,
