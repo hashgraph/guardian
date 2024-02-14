d3dc701cdde9d67a3d1e3cae20adc515655df42e
simvalery <valeriy.simonov@envisionblockchain.com>
2024-02-14 21:15:18 +0400

    fix frontend build
    Signed-off-by: simvalery <valeriy.simonov@envisionblockchain.com>

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -16,6 +16,47 @@ import 'codemirror/addon/fold/brace-fold';
 import 'codemirror/addon/fold/indent-fold';
 import 'codemirror/addon/edit/closebrackets';
 import 'codemirror/addon/edit/matchbrackets';
+import EventEmitter from 'events';
+import fs, { WatchOptions } from 'fs';
+
+declare class FSWatcher extends EventEmitter implements fs.FSWatcher{
+  options: WatchOptions
+
+  ref(): this; // <-- added
+  unref(): this; // <-- added
+
+  close(): void;
+
+  /**
+   * events.EventEmitter
+   *   1. change
+   *   2. error
+   */
+  addListener(event: string, listener: (...args: any[]) => void): this;
+  addListener(event: 'change', listener: (eventType: string, filename: string | Buffer) => void): this;
+  addListener(event: 'error', listener: (error: Error) => void): this;
+  addListener(event: 'close', listener: () => void): this;
+
+  on(event: string, listener: (...args: any[]) => void): this;
+  on(event: 'change', listener: (eventType: string, filename: string | Buffer) => void): this;
+  on(event: 'error', listener: (error: Error) => void): this;
+  on(event: 'close', listener: () => void): this;
+
+  once(event: string, listener: (...args: any[]) => void): this;
+  once(event: 'change', listener: (eventType: string, filename: string | Buffer) => void): this;
+  once(event: 'error', listener: (error: Error) => void): this;
+  once(event: 'close', listener: () => void): this;
+
+  prependListener(event: string, listener: (...args: any[]) => void): this;
+  prependListener(event: 'change', listener: (eventType: string, filename: string | Buffer) => void): this;
+  prependListener(event: 'error', listener: (error: Error) => void): this;
+  prependListener(event: 'close', listener: () => void): this;
+
+  prependOnceListener(event: string, listener: (...args: any[]) => void): this;
+  prependOnceListener(event: 'change', listener: (eventType: string, filename: string | Buffer) => void): this;
+  prependOnceListener(event: 'error', listener: (error: Error) => void): this;
+  prependOnceListener(event: 'close', listener: () => void): this;
+}
 
 if (environment.production) {
   enableProdMode();
