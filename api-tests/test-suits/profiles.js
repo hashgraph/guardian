189c84631b45074f969d091ffe4fd3e0f561ca52
Ihar <igor.tsykala@envisionblockchain.com>
2024-03-09 14:54:00 +0500

    feat: add esm for api-tests

--- api-tests/test-suits/profiles.js
+++ api-tests/test-suits/profiles.js
@@ -1,6 +1,7 @@
-const axios = require("axios");
-const { GetURL, GetToken } = require("../helpers");
-const assert = require("assert");
+import axios from 'axios';
+
+
+import { GetURL, GetToken } from '../helpers';
 
 function Profiles() {
     it('/profiles/balance', async function () {
@@ -79,6 +80,6 @@ function Profiles() {
     })
 }
 
-module.exports = {
-    Profiles
-}
+export {
+    Profiles,
+};
