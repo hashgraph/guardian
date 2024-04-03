9526072e8d6fa387ec4c391751c423f055021972
Celiant <nikita.nikishin@envisionblockchain.com>
2024-04-03 20:09:46 +0400

    added some tests for contracts and flow for irec4

--- e2e-tests/cypress/support/ApiUrls.js
+++ e2e-tests/cypress/support/ApiUrls.js
@@ -25,7 +25,10 @@ const API = {
     //Contracts
     ListOfContracts: "contracts/",
     ListOfPairs: "contracts/pairs/",
-    ListOfRequests: "contracts/retire/requests/",
+    RetireRequests: "contracts/retire/requests/",
+    WipeRequests: "contracts/wipe/requests/",
+    RetireContract: "contracts/retire/",
+    PoolContract: "pools/",
     //Modules
     ListOfAllModules: "modules/",
     ListOfModules: "modules/menu/",
@@ -67,11 +70,32 @@ const API = {
     PolicisImportMsgPreview: "policies/import/message/preview",
     PolicisImportMsgPreviewPush: "policies/push/import/message/preview",
     PolicyGroups: "groups/",
+    ChooseRegistrantRole: "tag/choose_role/blocks/",
+    CreateApplication: "tag/create_application/blocks",
+    WaitForApproveApplication: "tag/wait_for_approve",
+    DeviceGrid: "tag/devices_grid",
+    IssueRequestGrid: "tag/issue_requests_grid",
+    GetApplications: "tag/registrants_grid/blocks",
+    ApproveApplication: "tag/approve_registrant_btn/blocks",
+    ApproveRegistrantBtn: "tag/approve_registrant_btn",
+    CreateDevice: "tag/create_device_form/blocks",
+    GetDevices: "tag/approve_devices_grid/blocks",
+    ApproveDevice: "tag/approve_device_btn/blocks",
+    GetDeviceIssue: "tag/devices_grid/blocks",
+    GetIssues: "tag/issue_requests_grid(evident)/blocks",
+    ApproveIssueRequestsBtn: "tag/approve_issue_requests_btn/blocks",
+    CreateIssue: "tag/create_issue_request_form/blocks",
+
+
+
     //Artifacts
         Artifacts: "artifacts/",
     Tags: "tags/",
     //General
     ExportCSV: "export?type=csv",
-    ExportFile: "export/file"
+    ExportFile: "export/file",
+    Async: "push/",
+    Publish: "publish/",
+    Blocks: "blocks/"
 };
 export default API;
