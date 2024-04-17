28adc74866f28b6383bad4ec6866a3bf9dd5cb01
Ihar <igor.tsykala@envisionblockchain.com>
2024-04-17 15:57:11 +0500

    feat: add perf hooks in performance interceptor

--- api-gateway/src/helpers/interceptors/performance.ts
+++ api-gateway/src/helpers/interceptors/performance.ts
@@ -1,19 +1,21 @@
 import { Injectable, NestInterceptor, ExecutionContext, CallHandler } from '@nestjs/common';
 
 import { Observable, tap } from 'rxjs';
+import { performance } from 'perf_hooks';
 
 @Injectable()
 export class PerformanceInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<unknown> {
-    const start = Date.now();
+    const start: number = performance.now();
+
     const request = context.switchToHttp().getRequest();
-    const route = request.url;
+    const route = request?.url;
 
     return next.handle().pipe(
       tap(() => {
-        const end = Date.now();
-        const executionTime = end - start;
-        console.log(`Execution time for ${route}: ${executionTime}ms`);
+        const end: number = performance.now();
+        const executionTime: number = end - start;
+        console.log(`Execution time for ${route}: ${executionTime.toFixed(2)}ms`);
       }),
     );
   }
