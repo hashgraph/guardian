6a9aeae68a2e49198c00c4d02c87c698901327c4
Ihar <igor.tsykala@envisionblockchain.com>
2024-05-22 18:03:55 +0500

    feat: add turn off cache across env

--- api-gateway/src/helpers/decorators/cache.ts
+++ api-gateway/src/helpers/decorators/cache.ts
@@ -5,15 +5,22 @@ import { CacheInterceptor } from '../../helpers/interceptors/cache.js';
 
 //constants
 import { CACHE, META_DATA } from '../../constants/index.js';
+import process from 'process';
+
+const enableCache = process.env.ENABLE_CACHE;
 
 export function UseCache(
   { ttl = CACHE.DEFAULT_TTL, isExpress = false, isFastify = false, interceptors = [] }:
     { ttl?: number, isExpress?: boolean, isFastify?: boolean, interceptors?: any[] } = {}) {
 
-  return applyDecorators(
-    SetMetadata(META_DATA.EXPRESS, isExpress),
-    SetMetadata(META_DATA.FASTIFY, isFastify),
-    SetMetadata(META_DATA.TTL, ttl),
-    UseInterceptors(...[...interceptors, CacheInterceptor]),
-  );
+  if(enableCache) {
+    return applyDecorators(
+      SetMetadata(META_DATA.EXPRESS, isExpress),
+      SetMetadata(META_DATA.FASTIFY, isFastify),
+      SetMetadata(META_DATA.TTL, ttl),
+      UseInterceptors(...[...interceptors, CacheInterceptor]),
+    );
+  }
+
+  return applyDecorators();
 }
