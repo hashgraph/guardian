fba90a87ffe0a3e25e838945b37e2cf6362f9726
Celiant <nikita.nikishin@envisionblockchain.com>
2024-06-20 16:52:39 +0400

    update tests with modules and profiles

--- e2e-tests/cypress/e2e/api-tests/analytics/compareModules.cy.js
+++ e2e-tests/cypress/e2e/api-tests/analytics/compareModules.cy.js
@@ -1,8 +1,9 @@
 import { METHOD, STATUS_CODE } from "../../../support/api/api-const";
 import API from "../../../support/ApiUrls";
 
-context("Analytics",  { tags: '@analytics' },() => {
+context("Analytics", { tags: '@analytics' }, () => {
     const authorization = Cypress.env("authorization");
+    let moduleId1, moduleId2
     before(() => {
         cy.fixture("displayDocuments.module", "binary").then((binary) => Cypress.Blob.binaryStringToBlob(binary))
             .then((file) => {
@@ -17,6 +18,8 @@ context("Analytics",  { tags: '@analytics' },() => {
                     timeout: 180000,
                 }).then((response) => {
                     expect(response.status).to.eq(STATUS_CODE.SUCCESS);
+                    let json = JSON.parse(new TextDecoder("utf-8").decode(response.body))
+                    moduleId1 = json.id;
                 });
             });
         cy.request({
@@ -31,40 +34,30 @@ context("Analytics",  { tags: '@analytics' },() => {
             timeout: 180000
         }).then((response) => {
             expect(response.status).eql(STATUS_CODE.SUCCESS);
+            moduleId2 = response.body.id;
         });
     })
 
     it("Compare modules", () => {
-        let moduleId1, moduleId2
         cy.request({
-            method: METHOD.GET,
-            url: API.ApiServer + API.ListOfAllModules,
+            method: METHOD.POST,
+            url: API.ApiServer + API.ModuleCompare,
+            body: {
+                moduleId1: moduleId1,
+                moduleId2: moduleId2,
+                eventsLvl: 1,
+                propLvl: 2,
+                childrenLvl: 2,
+                idLvl: 0
+            },
             headers: {
                 authorization,
             }
         }).then((response) => {
-            moduleId1 = response.body.at(1)._id
-            moduleId2 = response.body.at(0)._id
-            cy.request({
-                method: METHOD.POST,
-                url: API.ApiServer + API.ModuleCompare,
-                body: {
-                    moduleId1: moduleId1,
-                    moduleId2: moduleId2,
-                    eventsLvl: "1",
-                    propLvl: "2",
-                    childrenLvl: "2",
-                    idLvl: "0"
-                },
-                headers: {
-                    authorization,
-                }
-            }).then((response) => {
-                expect(response.status).to.eq(STATUS_CODE.OK);
-                expect(response.body.left.id).to.eq(moduleId1);
-                expect(response.body.right.id).to.eq(moduleId2);
-                expect(response.body.total).not.null;
-            })
+            expect(response.status).to.eq(STATUS_CODE.OK);
+            expect(response.body.left.id).to.eq(moduleId1);
+            expect(response.body.right.id).to.eq(moduleId2);
+            expect(response.body.total).not.null;
         })
     });
 
@@ -75,10 +68,10 @@ context("Analytics",  { tags: '@analytics' },() => {
             body: {
                 moduleId1: "6419853a31fe4fd0e741b3a9",
                 moduleId2: "641983a931fe4fd0e741b399",
-                eventsLvl: "1",
-                propLvl: "2",
-                childrenLvl: "2",
-                idLvl: "0"
+                eventsLvl: 1,
+                propLvl: 2,
+                childrenLvl: 2,
+                idLvl: 0
             },
             headers: {
             },
@@ -96,10 +89,10 @@ context("Analytics",  { tags: '@analytics' },() => {
             body: {
                 moduleId1: "6419853a31fe4fd0e741b3a9",
                 moduleId2: "641983a931fe4fd0e741b399",
-                eventsLvl: "1",
-                propLvl: "2",
-                childrenLvl: "2",
-                idLvl: "0"
+                eventsLvl: 1,
+                propLvl: 2,
+                childrenLvl: 2,
+                idLvl: 0
             },
             headers: {
                 authorization: auth,
@@ -118,10 +111,10 @@ context("Analytics",  { tags: '@analytics' },() => {
             body: {
                 moduleId1: "6419853a31fe4fd0e741b3a9",
                 moduleId2: "641983a931fe4fd0e741b399",
-                eventsLvl: "1",
-                propLvl: "2",
-                childrenLvl: "2",
-                idLvl: "0"
+                eventsLvl: 1,
+                propLvl: 2,
+                childrenLvl: 2,
+                idLvl: 0
             },
             headers: {
                 authorization: auth,
@@ -133,34 +126,23 @@ context("Analytics",  { tags: '@analytics' },() => {
     });
 
     it("Compare modules(Export)", () => {
-        let moduleId1, moduleId2
         cy.request({
-            method: METHOD.GET,
-            url: API.ApiServer + API.ListOfAllModules,
+            method: METHOD.POST,
+            url: API.ApiServer + API.ModuleCompare + API.ExportCSV,
+            body: {
+                moduleId1: moduleId1,
+                moduleId2: moduleId2,
+                eventsLvl: 1,
+                propLvl: 2,
+                childrenLvl: 2,
+                idLvl: 0
+            },
             headers: {
                 authorization,
             }
         }).then((response) => {
-            moduleId1 = response.body.at(1)._id
-            moduleId2 = response.body.at(0)._id
-            cy.request({
-                method: METHOD.POST,
-                url: API.ApiServer + API.ModuleCompare + API.ExportCSV,
-                body: {
-                    moduleId1: moduleId1,
-                    moduleId2: moduleId2,
-                    eventsLvl: "1",
-                    propLvl: "2",
-                    childrenLvl: "2",
-                    idLvl: "0"
-                },
-                headers: {
-                    authorization,
-                }
-            }).then((response) => {
-                expect(response.status).to.eq(STATUS_CODE.OK);
-                expect(response.body).to.include("data:text/csv");
-            })
+            expect(response.status).to.eq(STATUS_CODE.OK);
+            expect(response.body).to.include("data:text/csv");
         })
     });
 
@@ -171,10 +153,10 @@ context("Analytics",  { tags: '@analytics' },() => {
             body: {
                 moduleId1: "6419853a31fe4fd0e741b3a9",
                 moduleId2: "641983a931fe4fd0e741b399",
-                eventsLvl: "1",
-                propLvl: "2",
-                childrenLvl: "2",
-                idLvl: "0"
+                eventsLvl: 1,
+                propLvl: 2,
+                childrenLvl: 2,
+                idLvl: 0
             },
             headers: {
             },
@@ -192,10 +174,10 @@ context("Analytics",  { tags: '@analytics' },() => {
             body: {
                 moduleId1: "6419853a31fe4fd0e741b3a9",
                 moduleId2: "641983a931fe4fd0e741b399",
-                eventsLvl: "1",
-                propLvl: "2",
-                childrenLvl: "2",
-                idLvl: "0"
+                eventsLvl: 1,
+                propLvl: 2,
+                childrenLvl: 2,
+                idLvl: 0
             },
             headers: {
                 authorization: auth,
@@ -214,10 +196,10 @@ context("Analytics",  { tags: '@analytics' },() => {
             body: {
                 moduleId1: "6419853a31fe4fd0e741b3a9",
                 moduleId2: "641983a931fe4fd0e741b399",
-                eventsLvl: "1",
-                propLvl: "2",
-                childrenLvl: "2",
-                idLvl: "0"
+                eventsLvl: 1,
+                propLvl: 2,
+                childrenLvl: 2,
+                idLvl: 0
             },
             headers: {
                 authorization: auth,
