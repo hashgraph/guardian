0aaba578037933ffb07376fcaf384aa90aee9733
Ihar <igor.tsykala@envisionblockchain.com>
2024-07-15 16:03:47 +0500

    feat: move queue service to pino logger[3834]

--- queue-service/src/app.ts
+++ queue-service/src/app.ts
@@ -1,4 +1,4 @@
-import { ApplicationState, COMMON_CONNECTION_CONFIG, DataBaseHelper, Logger, MessageBrokerChannel, NotificationService } from '@guardian/common';
+import { ApplicationState, COMMON_CONNECTION_CONFIG, DataBaseHelper, MessageBrokerChannel, NotificationService, PinoLogger, pinoLoggerInitialization } from '@guardian/common';
 import { ApplicationStates, GenerateUUIDv4 } from '@guardian/interfaces';
 import { MikroORM } from '@mikro-orm/core';
 import { MongoDriver } from '@mikro-orm/mongodb';
@@ -42,8 +42,10 @@ Promise.all([
 
     app.listen();
     const channel = new MessageBrokerChannel(cn, 'worker');
-    const logger = new Logger();
-    logger.setConnection(cn);
+
+    const logger: PinoLogger = await pinoLoggerInitialization(db);
+    // const logger = new Logger();
+    // logger.setConnection(cn);
     const state = new ApplicationState();
     await state.setServiceName('QUEUE').setConnection(cn).init();
     await state.updateState(ApplicationStates.STARTED);
@@ -51,7 +53,7 @@ Promise.all([
     await new QueueService().setConnection(cn).init();
 
     await state.updateState(ApplicationStates.READY);
-    logger.info('Queue service started', ['QUEUE_SERVICE'])
+    await logger.info('Queue service started', ['QUEUE_SERVICE'])
 
 }, (reason) => {
     console.log(reason);
