aa916f68d078e73d54ef9e5968b92ee9992dca59
simvalery <valeriy.simonov@envisionblockchain.com>
2024-07-17 15:20:13 +0400

    remove size from necessary params Signed-off-by: simvalery <valeriy.simonov@envisionblockchain.com>
    Signed-off-by: simvalery <valeriy.simonov@envisionblockchain.com>

--- policy-service/src/policy-engine/blocks/documents-source.ts
+++ policy-service/src/policy-engine/blocks/documents-source.ts
@@ -125,7 +125,7 @@ export class InterfaceDocumentsSource {
         let paginationData = null;
 
         if (pagination) {
-            if (itemsPerPage && page && size) {
+            if (itemsPerPage && page) {
                 await pagination.setState(user, {itemsPerPage, page, size});
             }
 
