6538b3ba183581a704f70a2e6992e8f593186ef1
Artem Buslaev <artem.buslaev@envisionblockchain.com>
2024-07-22 21:01:25 +0400

    fix loading map key on frontend side
    Signed-off-by: Artem Buslaev <artem.buslaev@envisionblockchain.com>

--- frontend/src/app/services/map.service.ts
+++ frontend/src/app/services/map.service.ts
@@ -1,7 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { API_BASE_URL } from './api';
+import { switchMap, tap } from 'rxjs/operators';
 
 /**
  * Services for working with map api.
@@ -10,8 +11,9 @@ import { API_BASE_URL } from './api';
 export class MapService {
     private readonly url: string = `${API_BASE_URL}/map`;
     private _mapLoaded: boolean = false;
+    private _mapRequested: boolean = false;
 
-    set mapLoaded(value: boolean) {
+    set mapLoaded(value) {
         this._mapLoaded = value;
     }
 
@@ -21,10 +23,30 @@ export class MapService {
 
     constructor(private http: HttpClient) {}
 
-    public getApiKey(): Observable<any> {
+    private _mapRequest(value?: string) {
+        return this.http.jsonp(
+            `https://maps.googleapis.com/maps/api/js${
+                value ? '?key=' + value : ''
+            }`,
+            'callback'
+        );
+    }
+
+    public getApiKey() {
         return this.http.get(`${this.url}/key`, {
             responseType: 'text',
         });
+    }
+
+    public loadMap(): Observable<any> {
+        if (this._mapRequested) {
+            return of(null);
+        }
+        this._mapRequested = true;
+        return this.getApiKey().pipe(
+            switchMap((key) => this._mapRequest(key)),
+            tap(() => (this.mapLoaded = true))
+        );
     }
 
     public getSentinelKey(): Observable<any> {
