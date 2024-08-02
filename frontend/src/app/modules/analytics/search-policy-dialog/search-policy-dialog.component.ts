5bc5f5c5c34de4785afe88f281d25d6e6bfc9214
Stepan Kiryakov <stepan.kiryakov@envisionblockchain.com>
2024-08-02 21:43:19 +0400

    fix
    Signed-off-by: Stepan Kiryakov <stepan.kiryakov@envisionblockchain.com>

--- frontend/src/app/modules/analytics/search-policy-dialog/search-policy-dialog.component.ts
+++ frontend/src/app/modules/analytics/search-policy-dialog/search-policy-dialog.component.ts
@@ -91,15 +91,15 @@ export class SearchPolicyDialog {
             this.filtersCount++;
         }
         if (filters.tokens) {
-            options.minTokensCount = filters.tokensCount || 0;
+            options.minTokensCount = filters.tokensCount || 1;
             this.filtersCount++;
         }
         if (filters.vcDocuments) {
-            options.minVcCount = filters.vcDocumentsCount || 0;
+            options.minVcCount = filters.vcDocumentsCount || 1;
             this.filtersCount++;
         }
         if (filters.vpDocuments) {
-            options.minVpCount = filters.vpDocumentsCount || 0;
+            options.minVpCount = filters.vpDocumentsCount || 1;
             this.filtersCount++;
         }
         this.error = null;
@@ -180,9 +180,9 @@ export class SearchPolicyDialog {
                 tokens: false,
                 vcDocuments: false,
                 vpDocuments: false,
-                tokensCount: 0,
-                vcDocumentsCount: 0,
-                vpDocumentsCount: 0
+                tokensCount: 1,
+                vcDocumentsCount: 1,
+                vpDocumentsCount: 1
             })
             this.load();
         }, 0);
@@ -197,9 +197,9 @@ export class SearchPolicyDialog {
             tokens: false,
             vcDocuments: false,
             vpDocuments: false,
-            tokensCount: 0,
-            vcDocumentsCount: 0,
-            vpDocumentsCount: 0
+            tokensCount: 1,
+            vcDocumentsCount: 1,
+            vpDocumentsCount: 1
         })
         this.select();
         this.load();
