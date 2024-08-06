eed7d06a61dc8f6869f2ae597825708a475eea4c
Artem Buslaev <artem.buslaev@envisionblockchain.com>
2024-08-06 20:58:28 +0400

    fix set options
    Signed-off-by: Artem Buslaev <artem.buslaev@envisionblockchain.com>

--- policy-service/src/policy-engine/helpers/set-options.ts
+++ policy-service/src/policy-engine/helpers/set-options.ts
@@ -12,15 +12,28 @@ export function setOptions(data: any, field: any, value: any) {
         for (let i = 0; i < keys.length - 1; i++) {
             const key = keys[i];
             if (key === 'L' && Array.isArray(result)) {
+                if (result.length > 0) {
+                    if (result[result.length - 1] === undefined) {
+                        result[result.length - 1] = {};
+                    }
+                } else {
+                    result.push({});
+                }
                 result = result[result.length - 1];
             } else {
+                if (result[key] === undefined) {
+                    result[key] = {};
+                }
                 result = result[key];
             }
-            if (result === undefined) {
-                result = {};
-            }
         }
-        result[keys[keys.length - 1]] = value;
+
+        if (Object.prototype.toString.call(result) !== '[object Object]') {
+            throw new Error('Can not set property on non object type');
+        }
+
+        const lastKey = keys[keys.length - 1];
+        result[lastKey] = value;
     }
     return data;
 }
