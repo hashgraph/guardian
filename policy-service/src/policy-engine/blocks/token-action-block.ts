0327f3206e7f90ab97874fc92db92ac5ba17d665
Artem Buslaev <artem.buslaev@envisionblockchain.com>
2024-08-29 16:27:52 +0300

    fix token action and token confirmation blocks
    Signed-off-by: Artem Buslaev <artem.buslaev@envisionblockchain.com>

--- policy-service/src/policy-engine/blocks/token-action-block.ts
+++ policy-service/src/policy-engine/blocks/token-action-block.ts
@@ -100,55 +100,30 @@ export class TokenActionBlock {
 
         const policyOwner = await PolicyUtils.getUserCredentials(ref, ref.policyOwner);
         const ownerCredentials = await policyOwner.loadHederaCredentials(ref);
-        const associatedAccountInfo = await PolicyUtils.getHederaAccountInfo(ref, account.hederaAccountId, ownerCredentials);
 
         switch (ref.options.action) {
             case 'associate': {
-                if (!associatedAccountInfo[token.tokenId]) {
-                    await PolicyUtils.associate(ref, token, account);
-                } else {
-                    console.warn('Token already associated', ref.policyId);
-                }
+                await PolicyUtils.associate(ref, token, account);
                 break;
             }
             case 'dissociate': {
-                if (associatedAccountInfo[token.tokenId]) {
-                    await PolicyUtils.dissociate(ref, token, account);
-                } else {
-                    console.warn('Token is not associated', ref.policyId);
-                }
+                await PolicyUtils.dissociate(ref, token, account);
                 break;
             }
             case 'freeze': {
-                if (associatedAccountInfo[token.tokenId] && associatedAccountInfo[token.tokenId].frozen === false) {
-                    await PolicyUtils.freeze(ref, token, account, ownerCredentials);
-                } else {
-                    console.warn('Can not freeze token: token is not associated or it is frozen or it does not support freeze action', ref.policyId);
-                }
+                await PolicyUtils.freeze(ref, token, account, ownerCredentials);
                 break;
             }
             case 'unfreeze': {
-                if (associatedAccountInfo[token.tokenId] && associatedAccountInfo[token.tokenId].frozen === true) {
-                    await PolicyUtils.unfreeze(ref, token, account, ownerCredentials);
-                } else {
-                    console.warn('Can not unfreeze token: token is not associated or it is not frozen or it does not support unfreeze action', ref.policyId);
-                }
+                await PolicyUtils.unfreeze(ref, token, account, ownerCredentials);
                 break;
             }
             case 'grantKyc': {
-                if (associatedAccountInfo[token.tokenId] && associatedAccountInfo[token.tokenId].kyc === false) {
-                    await PolicyUtils.grantKyc(ref, token, account, ownerCredentials);
-                } else if (associatedAccountInfo[token.tokenId]) {
-                    console.warn('Can not grant kyc token: token is not associated or it is granted kyc or it does not support grant kyc action', ref.policyId);
-                }
+                await PolicyUtils.grantKyc(ref, token, account, ownerCredentials);
                 break;
             }
             case 'revokeKyc': {
-                if (associatedAccountInfo[token.tokenId] && associatedAccountInfo[token.tokenId].kyc === true) {
-                    await PolicyUtils.revokeKyc(ref, token, account, ownerCredentials);
-                } else {
-                    console.warn('Can not grant kyc token: token is not associated or it is revoked kyc or it does not support revoke kyc action', ref.policyId);
-                }
+                await PolicyUtils.revokeKyc(ref, token, account, ownerCredentials);
                 break;
             }
             default:
