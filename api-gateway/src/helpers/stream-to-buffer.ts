fbc61682639afdff6ccb49f8935fa412c4a1a644
Ihar <igor.tsykala@envisionblockchain.com>
2024-09-17 18:52:55 +0500

    feat: cache serialization for streams

---
+++ api-gateway/src/helpers/stream-to-buffer.ts
@@ -0,0 +1,10 @@
+import { Readable } from 'stream';
+
+export function streamToBuffer(stream: Readable): Promise<Buffer> {
+    return new Promise((resolve, reject) => {
+        const chunks: Buffer[] = [];
+        stream.on('data', chunk => chunks.push(Buffer.from(chunk)));
+        stream.on('error', reject);
+        stream.on('end', () => resolve(Buffer.concat(chunks)));
+    });
+}
