cd51fb7ebf08892f0af5687185cd64dc53f12f38
Ihar <igor.tsykala@envisionblockchain.com>
2024-11-07 21:29:10 +0500

    feat: move to primeNg for table module[3141]

--- frontend/src/app/modules/common/material.module.ts
+++ frontend/src/app/modules/common/material.module.ts
@@ -1,3 +1,147 @@
+// import { NgModule } from '@angular/core';
+// import { CommonModule } from '@angular/common';
+// // import { MatLegacyTabsModule as MatTabsModule } from '@angular/material/legacy-tabs';
+// import { MatStepperModule } from '@angular/material/stepper';
+// // import { MatExpansionModule } from '@angular/material/expansion';
+// // import { MatIconModule } from '@angular/material/icon';
+// import { ReactiveFormsModule } from '@angular/forms';
+// // import { MatLegacyInputModule as MatInputModule } from '@angular/material/legacy-input';
+// // import { MatLegacyButtonModule as MatButtonModule } from '@angular/material/legacy-button';
+// import { MatLegacyTableModule as MatTableModule } from '@angular/material/legacy-table';
+// import { MatLegacyDialogModule as MatDialogModule } from '@angular/material/legacy-dialog';
+// import { MatLegacyProgressSpinnerModule as MatProgressSpinnerModule } from '@angular/material/legacy-progress-spinner';
+// import { MatToolbarModule } from '@angular/material/toolbar';
+// import { MatLegacyMenuModule as MatMenuModule } from '@angular/material/legacy-menu';
+// import { MatDividerModule } from '@angular/material/divider';
+// import { MatLegacyListModule as MatListModule } from '@angular/material/legacy-list';
+// import { MatLegacyProgressBarModule as MatProgressBarModule } from '@angular/material/legacy-progress-bar';
+// import { MatLegacySelectModule as MatSelectModule } from '@angular/material/legacy-select';
+// import { MatLegacyCheckboxModule as MatCheckboxModule } from '@angular/material/legacy-checkbox';
+// import { MatTreeModule } from '@angular/material/tree';
+// import { MatDatepickerModule } from '@angular/material/datepicker';
+// import { MatLegacyRadioModule as MatRadioModule } from '@angular/material/legacy-radio';
+// import { MatNativeDateModule } from '@angular/material/core';
+// import { MatLegacyTooltipModule as MatTooltipModule } from '@angular/material/legacy-tooltip';
+// import { MatLegacySlideToggleModule as MatSlideToggleModule } from '@angular/material/legacy-slide-toggle';
+// import { ClipboardModule } from '@angular/cdk/clipboard';
+// import { MatLegacyPaginatorModule as MatPaginatorModule } from '@angular/material/legacy-paginator';
+// import { MatSortModule } from '@angular/material/sort';
+// import { MatLegacyChipsModule as MatChipsModule } from '@angular/material/legacy-chips';
+// import { MatLegacyAutocompleteModule as MatAutocompleteModule } from '@angular/material/legacy-autocomplete';
+// import { MatButtonToggleModule } from '@angular/material/button-toggle';
+// import { DragDropModule } from '@angular/cdk/drag-drop';
+// import { CdkTableModule } from '@angular/cdk/table';
+// import { MatLegacySliderModule as MatSliderModule } from '@angular/material/legacy-slider';
+// import { MatBadgeModule } from '@angular/material/badge';
+// // import { TabViewModule } from 'primeng/tabview';
+// import { AccordionModule } from 'primeng/accordion';
+// import {TableModule} from 'primeng/table';
+// import {ButtonModule} from 'primeng/button';
+// import {MatButtonModule} from '@angular/material/button';
+// // import {InputTextModule} from 'primeng/inputtext';
+//
+// @NgModule({
+//     declarations: [],
+//     imports: [
+//         //angular modules
+//         CommonModule,
+//         ReactiveFormsModule,
+//         // MatTabsModule,
+//         // TabViewModule,
+//
+//         MatStepperModule,
+//
+//         // ButtonModule,
+//
+//         // MatExpansionModule,
+//         AccordionModule,
+//         // TableModule,
+//
+//         // MatIconModule,
+//
+//         // MatInputModule,
+//         // InputTextModule,
+//
+//         MatButtonModule,
+//
+//         MatTableModule,
+//         TableModule,
+//
+//         MatDialogModule,
+//         MatDatepickerModule,
+//         MatRadioModule,
+//         MatNativeDateModule,
+//         MatProgressSpinnerModule,
+//         MatToolbarModule,
+//         MatMenuModule,
+//         MatDividerModule,
+//         MatListModule,
+//         MatProgressBarModule,
+//         MatSelectModule,
+//         MatCheckboxModule,
+//         MatTreeModule,
+//         MatTooltipModule,
+//         MatSlideToggleModule,
+//         MatPaginatorModule,
+//         MatButtonToggleModule,
+//         DragDropModule,
+//         MatSliderModule,
+//         CdkTableModule,
+//         MatBadgeModule
+//     ],
+//     exports: [
+//         //angular modules
+//         ReactiveFormsModule,
+//         // MatTabsModule,
+//         // TabViewModule,
+//
+//         MatStepperModule,
+//
+//         // ButtonModule,
+//
+//         // MatExpansionModule,
+//         AccordionModule,
+//         // TableModule,
+//
+//         // MatIconModule,
+//
+//         // MatInputModule,
+//         // InputTextModule,
+//
+//         MatButtonModule,
+//
+//         MatTableModule,
+//         TableModule,
+//
+//         MatDialogModule,
+//         MatDatepickerModule,
+//         MatRadioModule,
+//         MatNativeDateModule,
+//         MatProgressSpinnerModule,
+//         MatToolbarModule,
+//         MatMenuModule,
+//         MatDividerModule,
+//         MatListModule,
+//         MatProgressBarModule,
+//         MatSelectModule,
+//         MatCheckboxModule,
+//         MatTreeModule,
+//         MatTooltipModule,
+//         MatSlideToggleModule,
+//         ClipboardModule,
+//         MatPaginatorModule,
+//         MatSortModule,
+//         MatChipsModule,
+//         MatAutocompleteModule,
+//         MatButtonToggleModule,
+//         DragDropModule,
+//         MatSliderModule,
+//         CdkTableModule,
+//         MatBadgeModule
+//     ]
+// })
+// export class MaterialModule { }
+
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 // import { MatLegacyTabsModule as MatTabsModule } from '@angular/material/legacy-tabs';
@@ -38,7 +182,6 @@ import { AccordionModule } from 'primeng/accordion';
 import {TableModule} from 'primeng/table';
 import {ButtonModule} from 'primeng/button';
 
-
 @NgModule({
     declarations: [],
     imports: [
@@ -52,11 +195,11 @@ import {ButtonModule} from 'primeng/button';
         AccordionModule,
         TableModule,
 
-        MatIconModule,
-        ButtonModule,
+        // MatIconModule,
+        // ButtonModule,
 
         ReactiveFormsModule,
-        MatInputModule,
+        // MatInputModule,
         MatButtonModule,
         MatTableModule,
         MatDialogModule,
@@ -91,13 +234,15 @@ import {ButtonModule} from 'primeng/button';
         AccordionModule,
         TableModule,
 
-        MatIconModule,
-        ButtonModule,
+        // MatIconModule,
+        // ButtonModule,
 
         ReactiveFormsModule,
         MatInputModule,
         MatButtonModule,
+
         MatTableModule,
+
         MatDialogModule,
         MatDatepickerModule,
         MatRadioModule,
--- frontend/src/app/modules/contract-engine/contract-engine.module.ts
+++ frontend/src/app/modules/contract-engine/contract-engine.module.ts
@@ -7,6 +7,7 @@ import { MaterialModule } from '../common/material.module';
 import { TagEngineModule } from '../tag-engine/tag-engine.module';
 import { AppRoutingModule } from 'src/app/app-routing.module';
 import {AngularSvgIconModule} from 'angular-svg-icon';
+import {TooltipModule} from 'primeng/tooltip';
 // Components
 import { RefreshBtnComponent } from './components/refresh-btn/refresh-btn.component';
 // Configs
@@ -44,7 +45,8 @@ import { UserContractConfigComponent } from './configs/user-contract-config/user
         MaterialModule,
         TagEngineModule,
         AppRoutingModule,
-        AngularSvgIconModule.forRoot()
+        AngularSvgIconModule.forRoot(),
+        TooltipModule,
     ],
     exports: [],
 })
