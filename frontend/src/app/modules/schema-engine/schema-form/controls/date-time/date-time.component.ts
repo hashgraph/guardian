023a8277ead21fb60e310c617e21923b567afbad
simvalery <valeriy.simonov@envisionblockchain.com>
2024-12-02 23:12:15 +0400

    fixes
    Signed-off-by: simvalery <valeriy.simonov@envisionblockchain.com>

--- frontend/src/app/modules/schema-engine/schema-form/controls/date-time/date-time.component.ts
+++ frontend/src/app/modules/schema-engine/schema-form/controls/date-time/date-time.component.ts
@@ -1,20 +1,29 @@
-import { AfterViewInit, Component, Input, NgZone, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, Input, NgZone, OnChanges, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { UntypedFormControl } from '@angular/forms';
 import * as moment from 'moment';
+import { Subject, Subscription } from 'rxjs';
 
 @Component({
     selector: 'date-time',
     templateUrl: './date-time.component.html',
     styleUrls: ['./date-time.component.scss'],
 })
-export class DateTimeComponent implements OnInit, AfterViewInit{
+export class DateTimeComponent implements OnInit, AfterViewInit, OnChanges, OnDestroy{
     @Input('control') control: UntypedFormControl;
     @Input('showIcon') showIcon: boolean = true;
     @Input('showSeconds') showSeconds: boolean = true;
     @Input('showTime') showTime: boolean = true;
+    @Input('timeOnly') timeOnly: boolean = false;
     @Input('dateFormat') dateFormat: string = 'yy-mm-dd';
+    @Input('item') item: any;
+    @Input('isMany') isMany: boolean = false;
+    @Input('index') isDisabled?: number;
+    @Input('update') update?: Subject<any>;
+    @Input('value') value?: string;
 
     @ViewChild('calendar') calendar: any
+
+    private subscription = new Subscription();
 
     constructor(
         private ngZone: NgZone,
@@ -22,14 +31,52 @@ export class DateTimeComponent implements OnInit, AfterViewInit{
     }
 
     ngOnInit() {
+        if (this.item.subject) {
+            this.item.subject.subscribe(() => {
+                this.fillField();
+            })
+        }
+    }
+
+    ngOnDestroy() {
+        this.subscription.unsubscribe();
     }
 
     ngAfterViewInit() {
-        const value = this.calendar.value;
-        if (value) {
-            this.ngZone.runOutsideAngular(() => {
-                this.calendar.el.nativeElement.querySelector('input').value = moment(value).format('YYYY-MM-DD HH:mm:ss');
-            });
+        if (this.isMany) {
+            this.fillField();
         }
+    }
+
+    ngOnChanges() {
+        this.fillField();
+    }
+
+    fillField() {
+        const comment = this.item?.field?.comment && JSON.parse(this.item.field.comment);
+        let value: any = null;
+        if (this.value) {
+            value = this.value;
+        } else if (this.timeOnly && comment?.suggest) {
+            value = comment.suggest;
+        } else if (this.calendar?.value) {
+            value = this.calendar?.value;
+        } else if (this.item.value) {
+            value = this.item.value;
+        }
+
+        const input = this.calendar?.el.nativeElement.querySelector('input')
+
+        setTimeout(() => {
+            if (input) {
+                if (this.timeOnly) {
+                    const date = moment(value, 'hh-mm-ss').toDate();
+                    this.control.setValue(date);
+                    input.value = value;
+                } else {
+                    input.value = moment(value).format('YYYY-MM-DD HH:mm:ss');
+                }
+            }
+        })
     }
 }
