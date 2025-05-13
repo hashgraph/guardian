bb0f0c3c9ac77e52fb51f0af74863ca9ba65b212
Stepan Kiryakov <stepan.kiryakov@envisionblockchain.com>
2025-05-13 21:50:13 +0400

    fix
    Signed-off-by: Stepan Kiryakov <stepan.kiryakov@envisionblockchain.com>

--- policy-service/src/policy-engine/actions-service.ts
+++ policy-service/src/policy-engine/actions-service.ts
@@ -298,7 +298,7 @@ export class PolicyActionsService {
         const collection = new DataBaseHelper(PolicyAction);
         let document: any;
         try {
-            const userMessageKey = await UserCredentials.loadMessageKeyByAccount(this.messageId, message.payer, null);
+            const userMessageKey = await UserCredentials.loadMessageKeyByAccount(this.messageId, message.accountId, null);
             await MessageServer.loadDocument(message, userMessageKey);
             document = message.getDocument();
         } catch (error) {
