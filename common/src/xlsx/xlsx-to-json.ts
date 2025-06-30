21e0c045e7c27b663b2092cb0b938bdf21c91221
egorenvisionblockchain <egor.anikey@envisionblockchain.com>
2025-07-01 00:01:27 +0200

    chore: bump packages and hide server header [4056] (#4999)
    Signed-off-by: egorenvisionblockchain <egor.anikey@envisionblockchain.com>

--- common/src/xlsx/xlsx-to-json.ts
+++ common/src/xlsx/xlsx-to-json.ts
@@ -717,31 +717,36 @@ export class XlsxToJson {
             value: any,
             invert: boolean
         } => {
+            const fnNode = tree as mathjs.FunctionNode;
+
             if (tree.type === 'FunctionNode') {
-                if (tree.fn.name === 'EXACT' && tree.args.length === 2) {
+                if (fnNode.fn.name === 'EXACT' && fnNode.args.length === 2) {
+                    const firstNode = fnNode.args[0] as mathjs.SymbolNode | mathjs.ConstantNode;
+                    const secondNode = fnNode.args[1] as mathjs.SymbolNode | mathjs.ConstantNode;
+
                     if (
-                        tree.args[0].type === 'SymbolNode' &&
-                        tree.args[1].type === 'ConstantNode'
+                        firstNode.type === 'SymbolNode' &&
+                        secondNode.type === 'ConstantNode'
                     ) {
                         return {
-                            fieldPath: tree.args[0].name,
-                            value: tree.args[1].value,
+                            fieldPath: firstNode.name,
+                            value: secondNode.value,
                             invert
                         };
                     }
                     if (
-                        tree.args[0].type === 'ConstantNode' &&
-                        tree.args[1].type === 'SymbolNode'
+                        firstNode.type === 'ConstantNode' &&
+                        secondNode.type === 'SymbolNode'
                     ) {
                         return {
-                            value: tree.args[0].value,
-                            fieldPath: tree.args[1].name,
+                            value: firstNode.value,
+                            fieldPath: secondNode.name,
                             invert
                         };
                     }
                 }
-                if (tree.fn.name === 'NOT' && tree.args.length === 1) {
-                    return parsFn(tree.args[0], true);
+                if (fnNode.fn.name === 'NOT' && fnNode.args.length === 1) {
+                    return parsFn(fnNode.args[0], true);
                 }
             }
             return null;
