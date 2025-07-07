ed068766ec0c6fbc287a0cf8cefcb0249d2ef57a
IharTsykala <sudja09@bk.ru>
2025-07-07 16:54:57 +0500

    Revert "Revert "refactor code[5069]""
    This reverts commit 6c231368a9f0aa8d17ea420b2f80c46eafc9ad96.

--- frontend/src/app/services/indexed-db-registry.service.ts
+++ frontend/src/app/services/indexed-db-registry.service.ts
@@ -1,7 +1,7 @@
 import {Injectable} from '@angular/core';
 import {openDB, IDBPDatabase} from 'idb';
 
-export interface StoreConfig {
+interface StoreConfig {
     name: string;
     options?: IDBObjectStoreParameters;
 }
