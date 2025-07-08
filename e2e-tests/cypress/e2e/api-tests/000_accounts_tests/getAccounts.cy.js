c2550b1993b26c94dcc01771c1b8618e5b178f3d
Celiant <nikita.nikishin@envisionblockchain.com>
2025-07-08 12:46:16 +0400

    debug tests order: run without tags

--- e2e-tests/cypress/e2e/api-tests/000_accounts_tests/getAccounts.cy.js
+++ e2e-tests/cypress/e2e/api-tests/000_accounts_tests/getAccounts.cy.js
@@ -7,8 +7,7 @@ context("Get accounts", { tags: ['accounts', 'firstPool', 'all'] }, () => {
     const SRUsername = Cypress.env('SRUser');
     const UserUsername = Cypress.env('User');
 
-    it("Get list of users", () => {        
-        cy.task('logFS')
+    it("Get list of users", () => {       
         Authorization.getAccessToken(SRUsername).then((authorization) => {
             cy.request({
                 method: METHOD.GET,
