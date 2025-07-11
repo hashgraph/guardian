90cf2bd8e090b0a210c6122b7e66c46a642c69b1
Stepan Kiryakov <stepan.kiryakov@envisionblockchain.com>
2025-07-11 15:10:40 +0400

    #5090
    Signed-off-by: Stepan Kiryakov <stepan.kiryakov@envisionblockchain.com>

--- frontend/src/app/modules/policy-engine/policy-configuration/policy-configuration/policy-configuration.component.ts
+++ frontend/src/app/modules/policy-engine/policy-configuration/policy-configuration/policy-configuration.component.ts
@@ -1261,7 +1261,7 @@ export class PolicyConfigurationComponent implements OnInit {
                 block,
                 folder: this.openFolder,
                 readonly: this.readonly,
-                policyId: this.policyId
+                policyId: this.rootId
             }
         });
         dialogRef.onClose.subscribe(async (result) => { });
