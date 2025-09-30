c28248fde9716a05337eb042e1b6a9aaddc82c41
Ihar <igor.tsykala@envisionblockchain.com>
2025-09-30 15:10:01 +0500

    feat: crud for tables files grid fs[5042]

--- common/src/entity/document-draft.ts
+++ common/src/entity/document-draft.ts
@@ -1,7 +1,8 @@
-import { Entity, Property, AfterDelete } from '@mikro-orm/core';
+import { Entity, Property, AfterDelete, BeforeCreate, BeforeUpdate, AfterUpdate } from '@mikro-orm/core';
 import { RestoreEntity } from '../models/index.js';
-import { DataBaseHelper } from '../helpers/index.js';
+import { DataBaseHelper, extractTableFileIds } from '../helpers/index.js';
 import { DeleteCache } from './delete-cache.js';
+import { ObjectId } from '@mikro-orm/mongodb';
 
 /**
  * Document draft
@@ -53,6 +54,86 @@ export class DocumentDraft extends RestoreEntity {
         nullable: false,
     })
     data: string;
+
+    /**
+     * Table File Ids
+     */
+    @Property({ nullable: true })
+    tableFileIds?: ObjectId[];
+
+    /**
+     * Old Table File Ids
+     */
+    @Property({ persist: false, nullable: true })
+    _oldTableFileIds?: ObjectId[];
+
+    @BeforeCreate()
+    async setDefaults() {
+        try {
+            const parsed = JSON.parse(this.data);
+            this.tableFileIds = extractTableFileIds(parsed);
+        } catch {
+            this.tableFileIds = undefined;
+        }
+    }
+
+    @BeforeUpdate()
+    async updateFiles() {
+        if (this.data.trim()) {
+            try {
+                const parsed = JSON.parse(this.data);
+                const nextTableFileIds = extractTableFileIds(parsed) || [];
+                const currentTableFileIds = this.tableFileIds || [];
+
+                const removedTableFileIds = currentTableFileIds.filter((existingId) => {
+                    const existing = String(existingId);
+                    return !nextTableFileIds.some((nextId) => String(nextId) === existing);
+                });
+
+                this._oldTableFileIds = removedTableFileIds.length ? removedTableFileIds : undefined;
+                this.tableFileIds = nextTableFileIds;
+            } catch {
+                if (this.tableFileIds && this.tableFileIds.length) {
+                    this._oldTableFileIds = this.tableFileIds;
+                }
+                this.tableFileIds = undefined;
+            }
+        } else {
+            if (this.tableFileIds && this.tableFileIds.length) {
+                this._oldTableFileIds = this.tableFileIds;
+            }
+            this.tableFileIds = undefined;
+        }
+    }
+
+    @AfterUpdate()
+    postUpdateFiles() {
+        if (this._oldTableFileIds && this._oldTableFileIds.length) {
+            for (const fileId of this._oldTableFileIds) {
+                DataBaseHelper.gridFS
+                    .delete(fileId)
+                    .catch((reason) => {
+                        console.error(`AfterUpdate: DocumentDraft, ${this._id}, _oldTableFileIds`);
+                        console.error(reason);
+                    });
+            }
+            delete this._oldTableFileIds;
+        }
+    }
+
+    @AfterDelete()
+    deleteFiles() {
+        if (this.tableFileIds && this.tableFileIds.length) {
+            for (const fileId of this.tableFileIds) {
+                DataBaseHelper.gridFS
+                    .delete(fileId)
+                    .catch((reason) => {
+                        console.error(`AfterDelete: DocumentDraft, ${this._id}, tableFileIds`);
+                        console.error(reason);
+                    });
+            }
+        }
+    }
 
     /**
      * Save delete cache
