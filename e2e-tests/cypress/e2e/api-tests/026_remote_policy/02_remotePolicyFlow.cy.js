0df174f9bb2848ebc736bf031c41d811cc54ce5f
Celiant <nikita.nikishin@envisionblockchain.com>
2025-10-03 11:46:42 +0400

    returned deleting mgs tenant

--- e2e-tests/cypress/e2e/api-tests/026_remote_policy/02_remotePolicyFlow.cy.js
+++ e2e-tests/cypress/e2e/api-tests/026_remote_policy/02_remotePolicyFlow.cy.js
@@ -326,21 +326,21 @@ context("Policies", { tags: ['remote_policy', 'secondPool', 'all'] }, () => {
         })
     })
 
-    // after('Delete MGS Tenant', () => {
-    //     Authorization.getAccessTokenMGS(MGSAdminUsername, null).then((authorization) => {
-    //         cy.request({
-    //             method: METHOD.POST,
-    //             url: API.ApiMGS + API.TenantsDelete,
-    //             headers: {
-    //                 authorization,
-    //             },
-    //             body: {
-    //                 tenantId: tenantId,
-    //                 tenantName: tenantName
-    //             }
-    //         }).then((response) => {
-    //             expect(response.status).to.eq(STATUS_CODE.OK);
-    //         })
-    //     })
-    // })
+    after('Delete MGS Tenant', () => {
+        Authorization.getAccessTokenMGS(MGSAdminUsername, null).then((authorization) => {
+            cy.request({
+                method: METHOD.POST,
+                url: API.ApiMGS + API.TenantsDelete,
+                headers: {
+                    authorization,
+                },
+                body: {
+                    tenantId: tenantId,
+                    tenantName: tenantName
+                }
+            }).then((response) => {
+                expect(response.status).to.eq(STATUS_CODE.OK);
+            })
+        })
+    })
 })
