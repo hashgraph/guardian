a949ac05ffe1ab25f07896d02d1b7c285c6df9d1
Stepan Kiryakov <stepan.kiryakov@envisionblockchain.com>
2025-10-14 14:55:33 +0400

    fix
    Signed-off-by: Stepan Kiryakov <stepan.kiryakov@envisionblockchain.com>

--- worker-service/src/api/ipfs-client-class.ts
+++ worker-service/src/api/ipfs-client-class.ts
@@ -159,8 +159,11 @@ export class IpfsClientClass {
 
                 try {
                     const garbageCollector = this.client.repo.gc();
-                    for await (const _ of garbageCollector) {}
-                } catch {}
+                    // tslint:disable-next-line:no-empty
+                    for await (const _ of garbageCollector) { }
+                } catch {
+                    return true;
+                }
 
                 return true;
             }
