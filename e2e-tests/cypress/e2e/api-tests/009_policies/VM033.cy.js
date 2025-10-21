dd9f2b6c29a232f2ccb97f417f88537dcc888ad6
Celiant <nikita.nikishin@envisionblockchain.com>
2025-10-21 17:01:36 +0400

    split all flow; added UI tests for GeoJSON and IPFS validation

--- e2e-tests/cypress/e2e/api-tests/009_policies/VM033.cy.js
+++ e2e-tests/cypress/e2e/api-tests/009_policies/VM033.cy.js
@@ -3,7 +3,7 @@ import API from "../../../support/ApiUrls";
 import * as Checks from "../../../support/checkingMethods";
 import * as Authorization from "../../../support/authorization";
 
-context("Policies", { tags: ['policies', 'secondPool'] }, () => {
+context("Policies", { tags: ['policies', 'secondPool', 'VM0033'] }, () => {
 
     const SRUsername = Cypress.env('SRUser');
     const PPUser = Cypress.env('PPUser');
