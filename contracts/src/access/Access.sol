a37f09863374e1334d6ffa31eed9e42a49ce481f
dima <nosid91@gmail.com>
2026-01-26 11:27:42 +0200

    Add ownership management tests, smart contract support, and CLI commands.
    Signed-off-by: dima <nosid91@gmail.com>

--- contracts/src/access/Access.sol
+++ contracts/src/access/Access.sol
@@ -2,11 +2,14 @@
 pragma solidity >=0.8.28 <0.9.0;
 
 abstract contract Access {
-    event OwnerAdded(address);
+    event OwnerAdded(address indexed account);
+    event OwnerRemoved(address indexed account);
+    event OwnerProposed(address indexed pendingOwner);
 
     bytes32 constant OWNER = keccak256("OWNER");
 
     mapping(bytes32 => mapping(address => bool)) roles;
+    address public pendingOwner;
 
     error NoPermissions();
 
@@ -27,6 +30,28 @@ abstract contract Access {
 
         _setRole(address(this), OWNER);
         emit OwnerAdded(address(this));
+    }
+
+    function proposeOwner(address newOwner) external role(OWNER) {
+        pendingOwner = newOwner;
+        emit OwnerProposed(newOwner);
+    }
+
+    function claimOwner() external {
+        if (msg.sender != pendingOwner) {
+            revert NoPermissions();
+        }
+        _setRole(msg.sender, OWNER);
+        emit OwnerAdded(msg.sender);
+        pendingOwner = address(0);
+    }
+
+    function removeOwner(address account) external role(OWNER) {
+        if (account == msg.sender || account == address(this)) {
+            revert NoPermissions();
+        }
+        _unsetRole(account, OWNER);
+        emit OwnerRemoved(account);
     }
 
     function _setRole(address usr, bytes32 r) internal {
