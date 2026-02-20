abb62aa13ea1c61a661f35995641f09694d18997
ruslanPL-EnvisionBlockchain <ruslan.pleskun@envisionblockchain.com>
2026-02-20 21:41:27 +0100

    fix: enable automated E2E test execution via Docker
    * Update e2e-tests documentation and Docker setup
    - Corrected links in README and added sections for running tests with tags, Docker setup, and CI/CD integration.
    - Enhanced the entrypoint script to automatically prepend the 'preparing' tag for user account creation.
    - Updated docker-compose configuration to improve environment variable handling and image naming.
    - Adjusted GitHub Actions workflow to ensure proper Docker image building and test result handling.
    * fix(*): enable automated e2e test execution via Docker
    * fix(*): update e2e-tests to be able to run them via Docker on Windows as well
    * increase balance and leave comment for remote policy tests
    ---------
    Co-authored-by: Celiant <nikita.nikishin@envisionblockchain.com>

--- e2e-tests/cypress/e2e/api-tests/026_remote_policy/00_remotePolicyUserPreparing.cy.js
+++ e2e-tests/cypress/e2e/api-tests/026_remote_policy/00_remotePolicyUserPreparing.cy.js
@@ -1,7 +1,7 @@
 import { METHOD, STATUS_CODE } from "../../../support/api/api-const";
 import API from "../../../support/ApiUrls";
 import * as Authorization from "../../../support/authorization";
-
+// Exclude "all" tag, if needs to stop using MGS in CI tests run
 context("Policies", { tags: ['remote_policy', 'secondPool', 'all'] }, () => {
 
     const MainSRUsername = Cypress.env('MainSRUser');
