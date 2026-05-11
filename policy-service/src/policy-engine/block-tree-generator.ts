861320369645c6f7166f750dfc4fd43b91c93049
OshanM <oshanm@xeptagon.com>
2026-05-11 16:27:49 +0530

    - Added executeSetBlockData method to remove duplicated code in policy events
    Signed-off-by: OshanM <oshanm@xeptagon.com>

--- policy-service/src/policy-engine/block-tree-generator.ts
+++ policy-service/src/policy-engine/block-tree-generator.ts
@@ -97,6 +97,81 @@ export class BlockTreeGenerator extends NatsService {
     }
 
     /**
+     * Shared execution path for SET_BLOCK_DATA and SET_BLOCK_DATA_BY_TAG.
+     */
+    private async executeSetBlockData(
+        block: IPolicyInterfaceBlock,
+        userFull: PolicyUser,
+        data: any,
+        policyId: string,
+        syncEvents: boolean | undefined,
+        history: boolean | undefined,
+        waitRemotePolicy: boolean | undefined,
+    ): Promise<MessageResponse<any> | MessageError<any>> {
+        // <-- Available
+        const error = await PolicyComponentsUtils.isAvailableSetData(block, userFull);
+        if (error) {
+            return error;
+        }
+        // Available -->
+
+        const baseEvent = {
+            blockType: block.blockType,
+            blockTag: block.tag,
+            blockId: block.uuid,
+            policyId,
+            userId: userFull.did,
+        };
+
+        const actionstep = new RecordActionStep(
+            (recordActionId, actionTimestemp, errors) => {
+                RecordUtils.RecordSetBlockData(policyId, userFull, block, data, recordActionId, actionTimestemp);
+                blockCompleteEvent({
+                    ...baseEvent,
+                    trackingId: recordActionId,
+                    status: errors.length === 0 ? 'success' : 'failure',
+                    error: errors[0]?.message,
+                    errorDetails: errors.length > 0 ? errors : undefined,
+                    timestamp: Date.now(),
+                } as IBlockCompleteEvent);
+            },
+            0, syncEvents, history
+        );
+
+        const res = await PolicyComponentsUtils.blockSetData(block, userFull, data, actionstep, waitRemotePolicy);
+
+        actionstep.finish();
+
+        if (res instanceof MessageError) {
+            actionstep.cancel();
+            blockCompleteEvent({
+                ...baseEvent,
+                trackingId: actionstep.id,
+                status: 'failure',
+                error: res.error,
+                errorDetails: [{ message: res.error }],
+                timestamp: Date.now(),
+            } as IBlockCompleteEvent);
+            return res;
+        }
+
+        if (syncEvents) {
+            const results = actionstep.getResults();
+            return new MessageResponse({
+                trackingId: actionstep.id,
+                response: res.body,
+                result: results.at(-1),
+                steps: history ? results : [],
+            }, res.code);
+        } else {
+            return new MessageResponse({
+                trackingId: actionstep.id,
+                ...(res.body ?? {}),
+            }, res.code);
+        }
+    }
+
+    /**
      * Init policy events
      */
     async initPolicyEvents(policyId: string, policyInstance: IPolicyInterfaceBlock, policy: Policy): Promise<void> {
@@ -199,71 +274,9 @@ export class BlockTreeGenerator extends NatsService {
                 waitRemotePolicy?: boolean
             }) => {
                 const { user, blockId, data, syncEvents, history, waitRemotePolicy } = msg;
-
                 const userFull = await this.getUser(policyInstance, user);
                 const block = PolicyComponentsUtils.GetBlockByUUID<IPolicyInterfaceBlock>(blockId);
-
-                // <-- Available
-                const error = await PolicyComponentsUtils.isAvailableSetData(block, userFull);
-                if (error) {
-                    return error;
-                }
-                // Available -->
-
-                const baseEvent = {
-                    blockType: block.blockType,
-                    blockTag: block.tag,
-                    blockId: block.uuid,
-                    policyId,
-                    userId: userFull.did,
-                };
-
-                const actionstep = new RecordActionStep(
-                    (recordActionId, actionTimestemp, errors) => {
-                        RecordUtils.RecordSetBlockData(policyId, userFull, block, data, recordActionId, actionTimestemp);
-                        blockCompleteEvent({
-                            ...baseEvent,
-                            trackingId: recordActionId,
-                            status: errors.length === 0 ? 'success' : 'failure',
-                            error: errors[0]?.message,
-                            errorDetails: errors.length > 0 ? errors : undefined,
-                            timestamp: Date.now(),
-                        } as IBlockCompleteEvent);
-                    },
-                    0, syncEvents, history
-                );
-
-                const res = await PolicyComponentsUtils.blockSetData(block, userFull, data, actionstep, waitRemotePolicy);
-
-                actionstep.finish();
-
-                if (res instanceof MessageError) {
-                    actionstep.cancel();
-                    blockCompleteEvent({
-                        ...baseEvent,
-                        trackingId: actionstep.id,
-                        status: 'failure',
-                        error: res.error,
-                        errorDetails: [{ message: res.error }],
-                        timestamp: Date.now(),
-                    } as IBlockCompleteEvent);
-                    return res;
-                }
-
-                if (syncEvents) {
-                    const results = actionstep.getResults();
-                    return new MessageResponse({
-                        trackingId: actionstep.id,
-                        response: res.body,
-                        result: results.at(-1),
-                        steps: history ? results : [],
-                    }, res.code);
-                } else {
-                    return new MessageResponse({
-                        trackingId: actionstep.id,
-                        ...(res.body ?? {}),
-                    }, res.code);
-                }
+                return this.executeSetBlockData(block, userFull, data, policyId, syncEvents, history, waitRemotePolicy);
             });
 
         this.getPolicyMessages(PolicyEvents.SET_BLOCK_DATA_BY_TAG, policyId,
@@ -279,68 +292,7 @@ export class BlockTreeGenerator extends NatsService {
                 const { user, tag, data, syncEvents, history, waitRemotePolicy } = msg;
                 const userFull = await this.getUser(policyInstance, user);
                 const block = PolicyComponentsUtils.GetBlockByTag<IPolicyInterfaceBlock>(policyId, tag);
-
-                // <-- Available
-                const error = await PolicyComponentsUtils.isAvailableSetData(block, userFull);
-                if (error) {
-                    return error;
-                }
-                // Available -->
-
-                const baseEvent = {
-                    blockType: block.blockType,
-                    blockTag: block.tag,
-                    blockId: block.uuid,
-                    policyId,
-                    userId: userFull.did,
-                };
-
-                const actionstep = new RecordActionStep(
-                    (recordActionId, actionTimestemp, errors) => {
-                        RecordUtils.RecordSetBlockData(policyId, userFull, block, data, recordActionId, actionTimestemp);
-                        blockCompleteEvent({
-                            ...baseEvent,
-                            trackingId: recordActionId,
-                            status: errors.length === 0 ? 'success' : 'failure',
-                            error: errors[0]?.message,
-                            errorDetails: errors.length > 0 ? errors : undefined,
-                            timestamp: Date.now(),
-                        } as IBlockCompleteEvent);
-                    },
-                    0, syncEvents, history
-                );
-
-                const res = await PolicyComponentsUtils.blockSetData(block, userFull, data, actionstep, waitRemotePolicy);
-
-                actionstep.finish();
-
-                if (res instanceof MessageError) {
-                    actionstep.cancel();
-                    blockCompleteEvent({
-                        ...baseEvent,
-                        trackingId: actionstep.id,
-                        status: 'failure',
-                        error: res.error,
-                        errorDetails: [{ message: res.error }],
-                        timestamp: Date.now(),
-                    } as IBlockCompleteEvent);
-                    return res;
-                }
-
-                if (syncEvents) {
-                    const results = actionstep.getResults();
-                    return new MessageResponse({
-                        trackingId: actionstep.id,
-                        response: res.body,
-                        result: results.at(-1),
-                        steps: history ? results : [],
-                    }, res.code);
-                } else {
-                    return new MessageResponse({
-                        trackingId: actionstep.id,
-                        ...(res.body ?? {}),
-                    }, res.code);
-                }
+                return this.executeSetBlockData(block, userFull, data, policyId, syncEvents, history, waitRemotePolicy);
             });
 
         this.getPolicyMessages(PolicyEvents.SELECT_POLICY_GROUP, policyId, async (msg: any) => {
