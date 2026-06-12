97a05bce2f019948812ab1d803551d9ce34dd652
Dariy Miseldzhani <dariy.miseldzhani@hashgraph.com>
2026-06-12 22:48:10 +0200

    feat(interfaces): add fieldPath and cross-schema targets to condition types
    Signed-off-by: Dariy Miseldzhani <dariy.miseldzhani@hashgraph.com>

--- interfaces/src/interface/schema-condition.interface.ts
+++ interfaces/src/interface/schema-condition.interface.ts
@@ -1,20 +1,34 @@
 import { SchemaField } from './schema-field.interface.js';
 
 /**
+ * A cross-schema condition target: a field that lives inside a nested sub-schema
+ * and is controlled by a parent schema's condition (not defined at the parent level).
+ * fieldPath is the full path from the parent schema root to the leaf field,
+ * e.g. ['fieldC_ref', 'NumberC'].
+ */
+export interface SchemaConditionTarget {
+  field: SchemaField;
+  fieldPath: string[];
+}
+
+/**
  * Schema condition
  */
 export interface SchemaCondition {
   ifCondition: (
-    { field: SchemaField; fieldValue: any } |
+    SchemaFieldPredicate |
     { AND: SchemaFieldPredicate[] } |
     { OR: SchemaFieldPredicate[] }
   );
   thenFields: SchemaField[];
   elseFields: SchemaField[];
+  thenTargets?: SchemaConditionTarget[];
+  elseTargets?: SchemaConditionTarget[];
   errors?: any[];
 }
 
 export interface SchemaFieldPredicate {
   field: SchemaField;
   fieldValue: any;
+  fieldPath?: string[];
 }
