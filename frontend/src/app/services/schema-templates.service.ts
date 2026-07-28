9858c1fef47206bcfe735c8f92cb3c8bc38fcbaa
Dariy Miseldzhani <dariy.miseldzhani@hashgraph.com>
2026-07-28 15:38:21 +0200

    feat(frontend): add schema templates service
    Signed-off-by: Dariy Miseldzhani <dariy.miseldzhani@hashgraph.com>

---
+++ frontend/src/app/services/schema-templates.service.ts
@@ -0,0 +1,43 @@
+import { HttpClient, HttpParams, HttpResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { ISchemaTemplate } from '@guardian/interfaces';
+import { Observable } from 'rxjs';
+import { API_BASE_URL } from './api';
+
+export interface SchemaTemplateGridItem extends ISchemaTemplate {
+    schemasCount?: number;
+}
+
+@Injectable()
+export class SchemaTemplatesService {
+    private readonly url: string = `${API_BASE_URL}/schema-templates`;
+
+    constructor(private readonly http: HttpClient) {
+    }
+
+    public page(pageIndex: number = 0, pageSize: number = 20): Observable<HttpResponse<SchemaTemplateGridItem[]>> {
+        const params = new HttpParams()
+            .set('pageIndex', String(pageIndex))
+            .set('pageSize', String(pageSize));
+        return this.http.get<SchemaTemplateGridItem[]>(this.url, {
+            observe: 'response',
+            params
+        });
+    }
+
+    public getById(id: string): Observable<SchemaTemplateGridItem> {
+        return this.http.get<SchemaTemplateGridItem>(`${this.url}/${id}`);
+    }
+
+    public create(template: Partial<ISchemaTemplate>): Observable<SchemaTemplateGridItem> {
+        return this.http.post<SchemaTemplateGridItem>(`${this.url}/`, template);
+    }
+
+    public update(id: string, template: Partial<ISchemaTemplate>): Observable<SchemaTemplateGridItem> {
+        return this.http.put<SchemaTemplateGridItem>(`${this.url}/${id}`, template);
+    }
+
+    public delete(id: string): Observable<boolean> {
+        return this.http.delete<boolean>(`${this.url}/${id}`);
+    }
+}
