403abb1bd69dcf9c4b28e6c4d87f7d1d6d983e1f
dariy-rised <82733639+dariy-rised@users.noreply.github.com>
2026-08-07 18:59:03 +0200

    fix: request blocks cache issue
    Signed-off-by: Dariy Miseldzhani <dariy.miseldzhani@hashgraph.com>

--- frontend/src/app/modules/policy-engine/policy-viewer/blocks/request-document-block-addon/request-document-block-addon.component.ts
+++ frontend/src/app/modules/policy-engine/policy-viewer/blocks/request-document-block-addon/request-document-block-addon.component.ts
@@ -107,6 +107,7 @@ export class RequestDocumentBlockAddonComponent
         const schemaRef = data?.schema;
         if (schemaRef && !schemaRef.document && schemaRef.id) {
             this.loading = true;
+            this.schemaService.evictSchemaById(schemaRef.id);
             this.schemaService.resolveSchemaById(schemaRef.id).subscribe({
                 next: (full) => {
                     data.schema = full;
